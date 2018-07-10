09c1cabb55dd8772141a98fa5c9d85e74bb1f004
Lars T. Kyllingstad <lars.kyllingstad@sintef.no>
2018-07-10 13:47:58 +0200

    Define a C API to run a one-slave execution
    This addresses issue #11.

--- include/cse.h
+++ include/cse.h
@@ -7,6 +7,7 @@
 #define CSE_H
 
 #include <stddef.h>
+#include <stdint.h>
 
 
 /// \defgroup csecorec C library
@@ -15,6 +16,192 @@
 extern "C" {
 #endif
 
+
+/// The type used to specify (simulation) time points.
+typedef double cse_time_point;
+
+/// The type used to specify (simulation) time durations.
+typedef double cse_time_duration;
+
+/// Variable index.
+typedef uint32_t cse_variable_index;
+
+/// Slave index.
+typedef int cse_slave_index;
+
+/// Error codes.
+typedef enum
+{
+    CSE_ERRC_SUCCESS = 0,
+
+    // --- Codes unique to the C API ---
+
+    /// Unspecified error (but message may contain details).
+    CSE_ERRC_UNSPECIFIED,
+
+    /// Error reported by C/C++ runtime; check `errno` to get the right code.
+    CSE_ERRC_ERRNO,
+
+    /// Invalid function argument.
+    CSE_ERRC_INVALID_ARGUMENT,
+
+    /// Index out of range.
+    CSE_ERRC_OUT_OF_RANGE,
+
+    /**
+     *  \brief
+     *  The time step failed, but can be retried with a shorter step length
+     *  (if supported by all slaves).
+     */
+    CSE_ERRC_STEP_TOO_LONG,
+
+    // --- Codes that correspond to C++ API error conditions ---
+
+    /// An input file is corrupted or invalid.
+    CSE_ERRC_BAD_FILE,
+
+    /// The requested feature (e.g. an FMI feature) is unsupported.
+    CSE_ERRC_UNSUPPORTED_FEATURE,
+
+    /// Error loading dynamic library (e.g. model code).
+    CSE_ERRC_DL_LOAD_ERROR,
+
+    /// The model reported an error.
+    CSE_ERRC_MODEL_ERROR,
+
+    /// ZIP file error.
+    CSE_ERRC_ZIP_ERROR,
+}
+cse_errc;
+
+
+/**
+ *  \brief
+ *  Returns the error code associated with the last reported error.
+ *
+ *  Most functions in this library will indicate that an error occurred by
+ *  returning -1 or `NULL`, after which this function can be called to
+ *  obtain more detailed information about the problem.
+ *
+ *  This function must be called from the thread in which the error occurred,
+ *  and before any new calls to functions in this library (with the exception
+ *  of `cse_last_error_message()`).
+ *
+ *  \returns
+ *      The error code associated with the last reported error.
+ */
+cse_errc cse_last_error_code();
+
+
+/**
+ *  \brief
+ *  Returns a textual description of the last reported error.
+ *
+ *  Most functions in this library will indicate that an error occurred by
+ *  returning -1 or `NULL`, after which this function can be called to
+ *  obtain more detailed information about the problem.
+ *
+ *  This function must be called from the thread in which the error occurred,
+ *  and before any new calls to functions in this library (with the exception
+ *  of `cse_last_error_code()`).
+ *
+ *  \returns
+ *      A textual description of the last reported error.  The pointer is
+ *      only guaranteed to remain valid until the next time a function in
+ *      this library is called (with the exception of `cse_last_error_code()`).
+ */
+const char* cse_last_error_message();
+
+
+struct cse_execution_s;
+
+/// An opaque object which contains the state for an execution.
+typedef struct cse_execution_s cse_execution;
+
+
+/**
+ *  \brief
+ *  Creates a new execution.
+ * 
+ *  \param [in] startTime
+ *      The (logical) time point at which the simulation should start.
+ *  \returns
+ *      A pointer to an object which holds the execution state,
+ *      or NULL on error.
+ */
+cse_execution* cse_execution_create(cse_time_point startTime);
+
+
+/**
+ *  \brief
+ *  Destroys an execution.
+ *
+ *  \returns
+ *      0 on success and -1 on error.
+ */
+int cse_execution_destroy(cse_execution* execution);
+
+
+/**
+ *  \brief
+ *  Loads a co-simulation FMU, instantiates a slave based on it, and adds it
+ *  to an execution.
+ *
+ *  The slave is owned by the execution and is destroyed along with it.
+ *
+ *  \note
+ *      Currently (and of course temporarily), only one slave may be added to
+ *      an execution.
+ *
+ *  \param [in] execution
+ *      The execution to which the slave should be added.
+ *  \param [in] fmuPath
+ *      The path to the FMU.
+ *
+ *  \returns
+ *      The slave's unique index in the execution, or -1 on error.
+ */
+cse_slave_index cse_execution_add_slave_from_fmu(
+    cse_execution* execution,
+    const char* fmuPath);
+
+
+/**
+ *  \brief
+ *  Advances an execution one time step.
+ *
+ *  \returns
+ *      0 on success and -1 on error.
+ */
+int cse_execution_step(cse_execution* execution, cse_time_duration stepSize);
+
+
+/**
+ *  \brief
+ *  Retrieves the values of real variables for one slave.
+ *
+ *  \param [in] execution
+ *      The execution.
+ *  \param [in] slave
+ *      The slave.
+ *  \param [in] variables
+ *      A pointer to an array of length `nv` that contains the (slave-specific)
+ *      indices of variables to retrieve.
+ *  \param [in] nv
+ *      The length of the `variables` and `values` arrays.
+ *  \param [out] values
+ *      A pointer to an array of length `nv` which will be filled with the
+ *      values of the variables specified in `variables`, in the same order.
+ *
+ *  \returns
+ *      0 on success and -1 on error.
+ */
+int cse_execution_slave_get_real(
+    cse_execution* execution,
+    cse_slave_index slave,
+    const cse_variable_index variables[],
+    size_t nv,
+    double values[]);
 
 
 /**
---
+++ test/c/single_fmu_execution_test.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <cse.h>
+
+
+void print_last_error()
+{
+    fprintf(stderr,
+        "Error code %d: %s\n",
+        cse_last_error_code(), cse_last_error_message());
+}
+
+
+int main()
+{
+    const char* dataDir = getenv("TEST_DATA_DIR");
+    if (!dataDir) {
+        fprintf(stderr, "Environment variable TEST_DATA_DIR not set\n");
+        return 1;
+    }
+
+    char fmuPath[1024];
+    int rc = snprintf(fmuPath, sizeof fmuPath, "%s/fmi1/identity.fmu", dataDir);
+    if (rc < 0) {
+        perror(NULL);
+        return 1;
+    }
+
+    cse_execution* execution = cse_execution_create(0.0);
+    if (!execution) {
+        print_last_error();
+        return 1;
+    }
+
+    cse_slave_index slave = cse_execution_add_slave_from_fmu(execution, fmuPath);
+    if (slave < 0) {
+        print_last_error();
+        cse_execution_destroy(execution);
+        return 1;
+    }
+
+    double dt = 0.1;
+    for (double t = 0.0; t <= 1.0; t += dt) {
+        rc = cse_execution_step(execution, dt);
+        if (rc < 0) {
+            print_last_error();
+            cse_execution_destroy(execution);
+            return 1;
+        }
+
+        cse_variable_index realOutVar = 0;
+        double realOutVal = -1.0;
+        rc = cse_execution_slave_get_real(
+            execution, slave,
+            &realOutVar, 1, &realOutVal);
+        if (rc < 0) {
+            print_last_error();
+            cse_execution_destroy(execution);
+            return 1;
+        }
+
+        if (realOutVal != 0.0) {
+            fprintf(stderr, "Expected value 0, got %f\n", realOutVal);
+            cse_execution_destroy(execution);
+            return 1;
+        }
+    }
+
+    rc = cse_execution_destroy(execution);
+    if (rc < 0) {
+        print_last_error();
+        return 1;
+    }
+
+    return 0;
+}
