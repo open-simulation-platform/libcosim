c594d38613c3ec77834bd972cf06aaa408f4472a
Lars T. Kyllingstad <lars.kyllingstad@sintef.no>
2018-07-17 08:26:50 +0200

    Upgrade to Boost 1.66.0
    The reason is that I ran into a C++17 compatibility issue with
    `boost::numeric_cast`, namely this one:
    https://github.com/boostorg/numeric_conversion/issues/5
    
    Note that the only reason to use Boost 1.65.1 in the first place was
    that it is the latest version supported by the the FindBoost script
    bundled with CMake 3.9.  Now, we're using a custom FindBoost script
    developed specifically for Bincrafters' modular Boost packages
    (the `cmake_findboost_modular` package), so this is a non-issue for us.

--- conanfile.py
+++ conanfile.py
@@ -7,12 +7,12 @@ class CSECoreConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
     requires = (
-        "boost_algorithm/1.65.1@bincrafters/stable",
-        "boost_log/1.65.1@bincrafters/stable",
-        "boost_property_tree/1.65.1@bincrafters/stable",
-        "boost_test/1.65.1@bincrafters/stable",
-        "boost_uuid/1.65.1@bincrafters/stable",
-        "cmake_findboost_modular/1.65.1@bincrafters/stable",
+        "boost_algorithm/1.66.0@bincrafters/stable",
+        "boost_log/1.66.0@bincrafters/stable",
+        "boost_property_tree/1.66.0@bincrafters/stable",
+        "boost_test/1.66.0@bincrafters/stable",
+        "boost_uuid/1.66.0@bincrafters/stable",
+        "cmake_findboost_modular/1.66.0@bincrafters/stable",
         "FMILibrary/2.0.3@kyllingstad/testing",
         "gsl_microsoft/1.0.0@bincrafters/stable",
         "libzip/1.5.1@bincrafters/stable"
