1ed6441f882b1cdb9af54036f0c58a4ef25d8cfb
Lars T. Kyllingstad <lars.kyllingstad@sintef.no>
2018-09-27 16:17:41 +0200

    Enable changing the output log level

--- include/cse/log.hpp
+++ include/cse/log.hpp
@@ -5,11 +5,13 @@
 #ifndef CSE_LOG_HPP
 #define CSE_LOG_HPP
 
+#include <ostream>
+
+
 namespace cse
 {
 namespace log
 {
-
 
 /// Severity levels for logged messages.
 enum class level
@@ -21,6 +23,32 @@ enum class level
     error,
     fatal
 };
+
+
+/// Returns a textual representation of `lvl`.
+constexpr const char* to_text(level lvl) noexcept
+{
+    switch (lvl) {
+        case level::trace: return "trace";
+        case level::debug: return "debug";
+        case level::info: return "info";
+        case level::warning: return "warning";
+        case level::error: return "error";
+        case level::fatal: return "fatal";
+        default: return nullptr;
+    }
+}
+
+
+/// Writes a textual representation of `lvl` to `stream`.
+inline std::ostream& operator<<(std::ostream& stream, level lvl)
+{
+    return stream << to_text(lvl);
+}
+
+
+/// Sets the global output log level.
+void set_global_output_level(level lvl);
 
 
 } // namespace log
--- src/cpp/cse/log/logger.hpp
+++ src/cpp/cse/log/logger.hpp
@@ -5,10 +5,12 @@
 #ifndef CSE_LOG_LOGGER_HPP
 #define CSE_LOG_LOGGER_HPP
 
+#include <boost/log/expressions/keyword.hpp>
 #include <boost/log/sources/global_logger_storage.hpp>
 #include <boost/log/sources/record_ostream.hpp>
 #include <boost/log/sources/severity_feature.hpp>
 #include <boost/log/sources/severity_logger.hpp>
+
 #include <cse/log.hpp>
 
 
@@ -17,10 +19,12 @@ namespace cse
 namespace log
 {
 
-
 BOOST_LOG_INLINE_GLOBAL_LOGGER_DEFAULT(
     logger,
     boost::log::sources::severity_logger_mt<level>)
+
+BOOST_LOG_ATTRIBUTE_KEYWORD(severity, "Severity", level)
+
 }
 } // namespace cse
 #endif // header guard
