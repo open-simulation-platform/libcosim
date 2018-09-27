15b78f03bc71ce1ab49b80cdd4a2e29833be4e8c
Lars T. Kyllingstad <lars.kyllingstad@sintef.no>
2018-09-27 16:17:41 +0200

    Enable changing the output log level

---
+++ src/cpp/log.cpp
@@ -0,0 +1,20 @@
+#include <cse/log.hpp>
+
+#include <boost/log/core.hpp>
+#include <boost/log/expressions.hpp>
+
+#include "cse/log/logger.hpp"
+
+
+namespace cse
+{
+namespace log
+{
+
+void set_global_output_level(level lvl)
+{
+    boost::log::core::get()->set_filter(severity >= lvl);
+}
+
+} // namespace log
+}
