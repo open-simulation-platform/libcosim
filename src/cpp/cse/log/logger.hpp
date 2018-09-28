4a17e9efa85d7565f791d5e2a0de195ba99fa98a
Lars T. Kyllingstad <lars.kyllingstad@sintef.no>
2018-09-28 15:53:13 +0200

    Set up custom logging sink
    The default logging sink in Boost.Log apparently does not support custom
    log level definitions, which was the cause of issue #48. This sets up a
    custom sink that writes to the standard log stream (which is really just
    the standard error stream) with custom message formatting, fixing that
    issue.
    
    The nice part is that this prepares the way for other custom sinks in
    the future, like log files, network logging or whatever.

--- src/cpp/cse/log/logger.hpp
+++ src/cpp/cse/log/logger.hpp
@@ -5,11 +5,10 @@
 #ifndef CSE_LOG_LOGGER_HPP
 #define CSE_LOG_LOGGER_HPP
 
-#include <boost/log/expressions/keyword.hpp>
-#include <boost/log/sources/global_logger_storage.hpp>
+#include <boost/log/sources/severity_logger.hpp>
+// These are just to introduce some convenient macros like BOOST_LOG_SEV:
 #include <boost/log/sources/record_ostream.hpp>
 #include <boost/log/sources/severity_feature.hpp>
-#include <boost/log/sources/severity_logger.hpp>
 
 #include <cse/log.hpp>
 
@@ -19,11 +18,10 @@ namespace cse
 namespace log
 {
 
-BOOST_LOG_INLINE_GLOBAL_LOGGER_DEFAULT(
-    logger,
-    boost::log::sources::severity_logger_mt<level>)
+using logger_type = boost::log::sources::severity_logger_mt<level>;
 
-BOOST_LOG_ATTRIBUTE_KEYWORD(severity, "Severity", level)
+/// Returns the global logger.
+logger_type& logger();
 
 }
 } // namespace cse
