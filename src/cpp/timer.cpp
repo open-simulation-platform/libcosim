36750cc3db1d2701435d1399b38b444e95c53b4c
Kristoffer Eide <37700866+eidekrist@users.noreply.github.com>
2018-10-02 12:50:06 +0200

    Fixing timer after merge of PR #52 (#55)
    * #52 Calling global logger correctly. Added underscores to private variables.
    
    * #54 Using duration helper types instead of duration constructor

--- src/cpp/timer.cpp
+++ src/cpp/timer.cpp
@@ -1,48 +1,46 @@
-#include <cse/timer.hpp>
 #include <cse/log.hpp>
 #include <cse/log/logger.hpp>
+#include <cse/timer.hpp>
+
 
 typedef std::chrono::steady_clock Time;
-constexpr std::chrono::duration<long, std::nano> MIN_SLEEP(100000L);
+constexpr std::chrono::microseconds MIN_SLEEP(100);
 
 namespace cse
 {
 
 real_time_timer::real_time_timer(cse::time_duration stepSize)
+    : stepDuration_(static_cast<long long>(stepSize * 1e9))
 {
-    long nanos = static_cast<long>(stepSize * 1e9);
-    stepDuration = std::chrono::duration<long>(nanos);
 }
 
 void real_time_timer::start()
 {
-    startTime = Time::now();
-    counter = 1L;
+    startTime_ = Time::now();
+    counter_ = 1L;
 }
 
 void real_time_timer::sleep()
 {
     Time::time_point current = Time::now();
-    Time::duration elapsed = current - startTime;
+    Time::duration elapsed = current - startTime_;
 
-    const std::chrono::duration<long, std::nano> expected(counter * stepDuration.count());
-    const std::chrono::duration<long, std::nano> totalSleep = expected - elapsed;
+    const std::chrono::nanoseconds expected(counter_ * stepDuration_.count());
+    const std::chrono::nanoseconds totalSleep = expected - elapsed;
 
     if (totalSleep > MIN_SLEEP) {
-        BOOST_LOG_SEV(log::logger::get(), log::level::trace)
-                << "Real time timer sleeping for "
-                << (std::chrono::duration_cast<std::chrono::milliseconds>(totalSleep)).count()
-                << " ms";
+        BOOST_LOG_SEV(log::logger(), log::level::trace)
+            << "Real time timer sleeping for "
+            << (std::chrono::duration_cast<std::chrono::milliseconds>(totalSleep)).count()
+            << " ms";
 
         std::this_thread::sleep_for(totalSleep);
     } else {
-        BOOST_LOG_SEV(log::logger::get(), log::level::debug)
+        BOOST_LOG_SEV(log::logger(), log::level::debug)
             << "Real time timer NOT sleeping, calculated sleep time "
-            << (std::chrono::duration_cast<std::chrono::nanoseconds>(totalSleep)).count() <<
-            " ns";
-
+            << totalSleep.count() << " ns";
     }
 
-    counter++;
+    counter_++;
 }
 } // namespace cse
