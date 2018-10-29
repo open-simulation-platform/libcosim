c257dfbe6402e2178e02fe854bd364cbecc9f9ac
Stenbro <Roger.Eivind.Stenbro@dnvgl.com>
2018-10-29 08:34:47 +0100

    opening stream in constructor + other cleanup

--- src/c/slave_logger.cpp
+++ src/c/slave_logger.cpp
@@ -7,16 +7,30 @@
 namespace cse
 {
 
-single_slave_logger::single_slave_logger(char* logPath)
-    : logPath_(logPath)
+single_slave_logger::single_slave_logger(char* logPath, int binary)
+    : logPath_(logPath), binary_(binary)
 {
+    if (binary == 1) {
+        fsw_ = std::ofstream(logPath_, std::ios_base::out | std::ios_base::binary);
+    } else {
+        fsw_ = std::ofstream(logPath_, std::ios_base::out | std::ios_base::app);
+    }
+
+    if (fsw_.fail()) {
+        throw std::runtime_error("Failed to open file stream for logging");
+    }
 }
 
-int single_slave_logger::write_int_samples(int* values, int nSamples, int binary)
+single_slave_logger::~single_slave_logger()
+{
+    fsw_.close();
+}
+
+int single_slave_logger::write_int_samples(int* values, int nSamples)
 {
     int rc = 0;
 
-    if (binary == 1) {
+    if (binary_ == 1) {
         rc = write_binary_ints(values);
     } else {
         rc = write_csv_ints(values, nSamples);
@@ -25,11 +39,11 @@ int single_slave_logger::write_int_samples(int* values, int nSamples, int binary
     return rc;
 }
 
-int single_slave_logger::write_real_samples(double* values, int nSamples, int binary)
+int single_slave_logger::write_real_samples(double* values, int nSamples)
 {
     int rc = 0;
 
-    if (binary == 1) {
+    if (binary_ == 1) {
         rc = write_binary_reals(values);
     } else {
         rc = write_csv_reals(values, nSamples);
@@ -38,84 +52,46 @@ int single_slave_logger::write_real_samples(double* values, int nSamples, int bi
     return rc;
 }
 
-
 int single_slave_logger::write_csv_ints(int* values, int nSamples)
 {
-    std::ofstream fsw(logPath_, std::ios_base::out | std::ios_base::app);
 
-    if (fsw.fail()) {
-        return -1;
+    if (fsw_.is_open()) {
+        for (int i = 0; i <= nSamples - 1; i++) {
+            fsw_ << values[i] << ",";
+        }
     }
-
-    for (int i = 0; i <= nSamples - 1; i++) {
-        fsw << values[i] << ",";
-    }
-
-    fsw << "\n";
 
     return 1;
 }
 
 int single_slave_logger::write_csv_reals(double* values, int nSamples)
 {
-    std::ofstream fsw(logPath_, std::ios_base::out | std::ios_base::app);
 
-    if (fsw.fail()) {
-        return -1;
+    if (fsw_.is_open()) {
+        for (int i = 0; i <= nSamples - 1; i++) {
+            fsw_ << values[i] << ",";
+        }
     }
-
-    for (int i = 0; i <= nSamples - 1; i++) {
-        fsw << values[i] << ",";
-    }
-
-    fsw << "\n";
 
     return 1;
 }
 
 int single_slave_logger::write_binary_ints(int* values)
 {
-    std::ofstream fsw(logPath_, std::ios_base::out | std::ios_base::app | std::ios_base::binary);
 
-    if (fsw.fail()) {
-        return -1;
+    if (fsw_.is_open()) {
+        fsw_.write((char*)&values, sizeof(values));
     }
-
-    fsw.write((char*)&values, sizeof(values));
 
     return 1;
 }
 
 int single_slave_logger::write_binary_reals(double* values)
 {
-    std::ofstream fsw(logPath_, std::ios_base::out | std::ios_base::app | std::ios_base::binary);
 
-    if (fsw.fail()) {
-        return -1;
+    if (fsw_.is_open()) {
+        fsw_.write((char*)&values, sizeof(values));
     }
-
-    fsw.write((char*)&values, sizeof(values));
-
-    return 1;
-}
-
-int single_slave_logger::memory_mapped_binary_write(double* values)
-{
-    boost::interprocess::file_mapping::remove(logPath_);
-
-    std::filebuf buf;
-    buf.open(logPath_, std::ios_base::out | std::ios_base::trunc | std::ios_base::binary);
-    buf.pubseekoff(sizeof(values), std::ios_base::beg);
-    buf.sputc(0);
-
-    boost::interprocess::file_mapping mapping(logPath_, boost::interprocess::read_write);
-    boost::interprocess::mapped_region region(mapping, boost::interprocess::read_write);
-
-    void* addr = region.get_address();
-
-    memcpy(addr, &values, sizeof(values));
-
-    region.flush();
 
     return 1;
 }
