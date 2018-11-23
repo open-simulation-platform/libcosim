c452ecb312acc57440e8fe7e3e983275b34b157d
Roger Eivind Stenbro <Roger.Eivind.Stenbro@dnvgl.com>
2018-11-23 13:07:52 +0100

    #96 Tests now working

--- test/cpp/membuffer_observer_test.cpp
+++ test/cpp/membuffer_observer_test.cpp
@@ -12,10 +12,11 @@
 #define REQUIRE(test) \
     if (!(test)) throw std::runtime_error("Requirement not satisfied: " #test)
 
-int main() {
+int main()
+{
     try {
         constexpr cse::time_point startTime = cse::to_time_point(0.0);
-        constexpr cse::time_point endTime = cse::to_time_point(10.0);
+        constexpr cse::time_point endTime = cse::to_time_point(3.0);
         constexpr cse::duration stepSize = cse::to_duration(0.1);
 
         cse::log::set_global_output_level(cse::log::level::debug);
@@ -28,16 +29,28 @@ int main() {
         execution.add_observer(observer);
 
         // Add slaves to the execution and connect variables
-        execution.add_slave(
-                cse::make_pseudo_async(std::make_unique<mock_slave>([](double x) { return x + 1.234; })), "slave uno");
-        execution.add_slave(
-                cse::make_pseudo_async(std::make_unique<mock_slave>([](double x) { return x + 1.234; }, [](int y) { return y - 4; })), "slave dos");
+        auto simIndex = execution.add_slave(
+            cse::make_pseudo_async(std::make_unique<mock_slave>([](double x) { return x + 1.234; })), "slave uno");
 
-        /*
         // Run the simulation
         auto simResult = execution.simulate_until(endTime);
         REQUIRE(simResult.get());
-        */
+
+        const int numSamples = 30;
+        const cse::variable_index varIndex = 0;
+        double realValues[numSamples];
+        cse::step_number steps[numSamples];
+
+        observer->get_real_samples(simIndex, varIndex, 0, gsl::make_span(realValues, numSamples), gsl::make_span(steps, numSamples));
+
+        double expectedReals[] = {0, 1.234, 1.234, 1.234, 1.234, 1.234, 1.234, 1.234, 1.234, 1.234, 1.234, 1.234, 1.234, 1.234, 1.234, 1.234, 1.234, 1.234
+                , 1.234, 1.234, 1.234, 1.234, 1.234, 1.234, 1.234, 1.234, 1.234, 1.234, 1.234, 1.234};
+
+        int counter=0;
+        for (double value : realValues) {
+            REQUIRE(std::fabs(value - expectedReals[counter]) < 1.0e-9);
+            counter++;
+        }
 
     } catch (const std::exception& e) {
         std::cerr << "Error: " << e.what() << std::endl;
