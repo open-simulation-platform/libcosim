5b7b529e1b6877fcb041865999a78672984ebc18
Jamt, Levi Kristian <Levi.Jamt@dnvgl.com>
2018-12-04 15:39:42 +0100

    #100 Move ssp_parser into load_ssp() function

--- include/cse/ssp_parser.hpp
+++ include/cse/ssp_parser.hpp
@@ -2,89 +2,16 @@
 #ifndef CSECORE_SSP_PARSER_HPP
 #define CSECORE_SSP_PARSER_HPP
 
-#include <boost/foreach.hpp>
-#include <boost/property_tree/ptree.hpp>
-#include <boost/property_tree/xml_parser.hpp>
-#include <exception>
-#include <string>
-
 #include <boost/filesystem/path.hpp>
 
-#include <cse/algorithm.hpp>
-#include <cse/fmi/fmu.hpp>
+#include <cse/execution.hpp>
+#include <cse/model.hpp>
 
 
 namespace cse
 {
 
-class ssp_parser
-{
-
-public:
-    ssp_parser(boost::filesystem::path xmlPath);
-    ~ssp_parser() noexcept;
-
-    struct DefaultExperiment
-    {
-        double startTime;
-        double stopTime;
-    };
-
-    const DefaultExperiment &get_default_experiment() const;
-
-    struct SimulationInformation
-    {
-        std::string description;
-        double stepSize;
-    };
-
-    const SimulationInformation &get_simulation_information() const;
-
-    struct SystemDescription
-    {
-        std::string name;
-        std::string version;
-        std::string systemName;
-        std::string systemDescription;
-    };
-
-    struct Connector
-    {
-        std::string name;
-        std::string kind;
-        std::string type;
-    };
-
-    struct Component
-    {
-        std::string name;
-        std::string source;
-        std::vector<Connector> connectors;
-    };
-
-    const std::vector<Component> &get_elements() const;
-
-    struct Connection
-    {
-        std::string startElement;
-        std::string startConnector;
-        std::string endElement;
-        std::string endConnector;
-    };
-
-    const std::vector<Connection> &get_connections() const;
-
- private:
-    boost::filesystem::path xmlPath_;
-    boost::property_tree::ptree pt_;
-
-    SystemDescription systemDescription_;
-    DefaultExperiment defaultExperiment_;
-    SimulationInformation simulationInformation_;
-    std::vector<Component> elements_;
-    std::vector<Connection> connections_;
-
-};
+execution load_ssp(const boost::filesystem::path& sspDir, cse::time_point startTime);
 
 } // namespace cse
 
