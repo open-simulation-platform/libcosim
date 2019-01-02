b90ab03447daa67cea4645d9b17f2fd4a365c94b
Lars T. Kyllingstad <lars.kyllingstad@sintef.no>
2019-01-02 07:39:59 +0100

    Add utility::shared_box

---
+++ src/cpp/cse/utility/concurrency.hpp
@@ -0,0 +1,91 @@
+/**
+ *  \file
+ *  \brief Concurrency utilities.
+ */
+#ifndef CSE_UTILITY_CONCURRENCY
+#define CSE_UTILITY_CONCURRENCY
+
+#include <mutex>
+#include <optional>
+
+#include <boost/fiber/condition_variable.hpp>
+#include <boost/fiber/mutex.hpp>
+
+
+namespace cse
+{
+namespace utility
+{
+
+
+/**
+ *  A thread-safe, fiber-friendly, single-item container.
+ *
+ *  This is a general-purpose container that may contain zero or one item(s)
+ *  of type `T`.
+ *
+ *  The `put()` and `take()` functions can be safely called from different
+ *  threads.  Shared data is protected with synchronization primitives
+ *  from Boost.Fiber, meaning that the functions will yield to other fibers,
+ *  and not block the current thread, if they cannot immediately acquire a
+ *  lock.
+ */
+template<typename T>
+class shared_box
+{
+public:
+    using value_type = T;
+
+    /// Puts an item in the container, replacing any existing item.
+    void put(const value_type& value)
+    {
+        {
+            std::lock_guard<boost::fibers::mutex> lock(mutex_);
+            value_ = value;
+        }
+        condition_.notify_one();
+    }
+
+    /// Puts an item in the container, replacing any existing item.
+    void put(value_type&& value)
+    {
+        {
+            std::lock_guard<boost::fibers::mutex> lock(mutex_);
+            value_ = std::move(value);
+        }
+        condition_.notify_one();
+    }
+
+    /**
+     *  Removes an item from the container and returns it.
+     *
+     *  If there is no item in the container when the function is called,
+     *  the current fiber will yield, and it will only resume when an
+     *  item becomes available.
+     */
+    value_type take()
+    {
+        std::unique_lock<boost::fibers::mutex> lock(mutex_);
+        condition_.wait(lock, [&] { return value_.has_value(); });
+        auto value = std::move(value_.value());
+        value_.reset();
+        return std::move(value);
+    }
+
+    /// Returns `true` if there is no item in the container.
+    bool empty() const noexcept
+    {
+        std::lock_guard<boost::fibers::mutex> lock(mutex_);
+        return !value_.has_value();
+    }
+
+private:
+    std::optional<value_type> value_;
+    mutable boost::fibers::mutex mutex_;
+    boost::fibers::condition_variable condition_;
+};
+
+
+} // namespace utility
+} // namespace cse
+#endif // header guard
---
+++ test/cpp/utility_concurrency_unittest.cpp
@@ -0,0 +1,65 @@
+#define BOOST_TEST_MODULE cse::utility shared_queue unittests
+#include <boost/test/unit_test.hpp>
+#include <cse/utility/concurrency.hpp>
+#include <thread>
+#include <vector>
+
+
+BOOST_AUTO_TEST_CASE(shared_box_copyable)
+{
+    constexpr int itemCount = 1000;
+    cse::utility::shared_box<int> box;
+
+    // reader
+    std::thread reader([&]() {
+        for (int i = 0; i < itemCount; ++i) {
+            BOOST_TEST(box.take() == i);
+        }
+    });
+
+    // writer
+    for (int i = 0; i < itemCount;) {
+        if (box.empty()) box.put(i++);
+    }
+
+    reader.join();
+}
+
+
+namespace
+{
+struct noncopyable
+{
+    explicit noncopyable(int n)
+        : number(n)
+    {}
+
+    noncopyable(const noncopyable&) = delete;
+    noncopyable& operator=(const noncopyable&) = delete;
+    noncopyable(noncopyable&&) = default;
+    noncopyable& operator=(noncopyable&&) = default;
+
+    int number = 0;
+};
+} // namespace
+
+
+BOOST_AUTO_TEST_CASE(shared_box_noncopyable)
+{
+    constexpr int itemCount = 1000;
+    cse::utility::shared_box<noncopyable> box;
+
+    // reader
+    std::thread reader([&]() {
+        for (int i = 0; i < itemCount; ++i) {
+            BOOST_TEST(box.take().number == i);
+        }
+    });
+
+    // writer
+    for (int i = 0; i < itemCount;) {
+        if (box.empty()) box.put(noncopyable(i++));
+    }
+
+    reader.join();
+}
