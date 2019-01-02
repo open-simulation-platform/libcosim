2786f40e1018579cb338bac6b13e70a803b7743a
Lars T. Kyllingstad <lars.kyllingstad@sintef.no>
2019-01-02 07:38:39 +0100

    Improve exception safety in pseudo_async_slave
    The state variable (`state_`) in `pseudo_async_slave` is now managed by
    an RAII object of type `state_guard`.  This reduces the potential for
    exception-safety bugs and removes some boilerplate code from the async
    functions in `pseudo_async_slave`.
    
    All this will really pay off when `state_guard` is re-used for other
    `async_slave` implementations.

--- src/cpp/async_slave.cpp
+++ src/cpp/async_slave.cpp
@@ -1,5 +1,7 @@
 #include <cse/async_slave.hpp>
 
+#include <exception>
+#include <sstream>
 #include <type_traits>
 #include <utility>
 
@@ -8,6 +10,7 @@
 #include <boost/fiber/future/async.hpp>
 
 #include "cse/error.hpp"
+#include "cse/exception.hpp"
 
 
 namespace cse
@@ -15,7 +18,87 @@ namespace cse
 namespace
 {
 
-/*
+/**
+ *  Helper class which checks, sets and resets the state variable for
+ *  an `async_slave`.
+ *
+ *  The constructors of this class take a reference to the `slave_state`
+ *  variable to be managed, and immediately set it to `indeterminate`.
+ *  On destruction, the managed variable will be automatically set to
+ *  a specified value, or, if an exception is currently "in flight", to
+ *  the special `error` value.
+ */
+class state_guard
+{
+public:
+    /**
+     *  Constructs a `state_guard` that sets `stateVariable` to `finalState`
+     *  on destruction.
+     */
+    state_guard(slave_state& stateVariable, slave_state finalState)
+        : stateVariable_(&stateVariable)
+        , finalState_(finalState)
+    {
+        stateVariable = slave_state::indeterminate;
+    }
+
+    /**
+     *  Constructs a `state_guard` that resets `stateVariable` to its original
+     *  value on destruction.
+     */
+    state_guard(slave_state& stateVariable)
+        : state_guard(stateVariable, stateVariable)
+    {}
+
+    /**
+     *  Manually sets the managed variable to its final value and relinquishes
+     *  control of it.  Does not check for exceptions.
+     */
+    void reset() noexcept
+    {
+        if (stateVariable_ != nullptr) {
+            *stateVariable_ = finalState_;
+            stateVariable_ = nullptr;
+        }
+    }
+
+    ~state_guard() noexcept
+    {
+        if (stateVariable_ != nullptr) {
+            if (std::uncaught_exceptions()) {
+                *stateVariable_ = slave_state::error;
+            } else {
+                *stateVariable_ = finalState_;
+            }
+        }
+    }
+
+    // Disallow copying, so that we don't inadvertently end up in a situation
+    // where multiple `state_guard` objects try to manage the same state.
+    state_guard(const state_guard&) = delete;
+    state_guard& operator=(const state_guard&) = delete;
+
+    state_guard(state_guard&& other) noexcept
+        : stateVariable_(other.stateVariable_)
+        , finalState_(other.finalState_)
+    {
+        other.stateVariable_ = nullptr;
+    }
+    state_guard& operator=(state_guard&& other) noexcept
+    {
+        stateVariable_ = other.stateVariable_;
+        finalState_ = other.finalState_;
+        other.stateVariable_ = nullptr;
+        return *this;
+    }
+
+private:
+    slave_state* stateVariable_;
+    slave_state finalState_;
+};
+
+
+/**
  *  Runs `f` in a new fiber with a bigger-than-default stack size.
  *
  *  The default fiber stack size in Boost.Fiber is just a few kilobytes.
@@ -70,18 +153,8 @@ public:
         CSE_PRECONDITION(
             state_ != slave_state::error &&
             state_ != slave_state::indeterminate);
-        const auto oldState = state_;
-        state_ = slave_state::indeterminate;
-
-        return big_stack_async([=]() {
-            try {
-                const auto result = slave_->model_description();
-                state_ = oldState;
-                return result;
-            } catch (...) {
-                state_ = slave_state::error;
-                throw;
-            }
+        return big_stack_async([=, _ = state_guard(state_)] {
+            return slave_->model_description();
         });
     }
 
@@ -92,48 +165,24 @@ public:
         override
     {
         CSE_PRECONDITION(state_ == slave_state::created);
-        state_ = slave_state::indeterminate;
-
-        return big_stack_async([=]() {
-            try {
-                slave_->setup(startTime, stopTime, relativeTolerance);
-                state_ = slave_state::initialisation;
-            } catch (...) {
-                state_ = slave_state::error;
-                throw;
-            }
+        return big_stack_async([=, _ = state_guard(state_, slave_state::initialisation)] {
+            slave_->setup(startTime, stopTime, relativeTolerance);
         });
     }
 
     boost::fibers::future<void> start_simulation() override
     {
         CSE_PRECONDITION(state_ == slave_state::initialisation);
-        state_ = slave_state::indeterminate;
-
-        return big_stack_async([=]() {
-            try {
-                slave_->start_simulation();
-                state_ = slave_state::simulation;
-            } catch (...) {
-                state_ = slave_state::error;
-                throw;
-            }
+        return big_stack_async([=, _ = state_guard(state_, slave_state::simulation)] {
+            slave_->start_simulation();
         });
     }
 
     boost::fibers::future<void> end_simulation() override
     {
         CSE_PRECONDITION(state_ == slave_state::simulation);
-        state_ = slave_state::indeterminate;
-
-        return big_stack_async([=]() {
-            try {
-                slave_->end_simulation();
-                state_ = slave_state::terminated;
-            } catch (...) {
-                state_ = slave_state::error;
-                throw;
-            }
+        return big_stack_async([=, _ = state_guard(state_, slave_state::terminated)] {
+            slave_->end_simulation();
         });
     }
 
@@ -143,17 +192,8 @@ public:
         override
     {
         CSE_PRECONDITION(state_ == slave_state::simulation);
-        state_ = slave_state::indeterminate;
-
-        return big_stack_async([=]() {
-            try {
-                const auto result = slave_->do_step(currentT, deltaT);
-                state_ = slave_state::simulation;
-                return result;
-            } catch (...) {
-                state_ = slave_state::error;
-                throw;
-            }
+        return big_stack_async([=, _ = state_guard(state_)] {
+            return slave_->do_step(currentT, deltaT);
         });
     }
 
@@ -175,33 +215,25 @@ public:
         booleanBuffer_.resize(booleanVariables.size());
         stringBuffer_.resize(stringVariables.size());
 
-        const auto oldState = state_;
-        state_ = slave_state::indeterminate;
-
         return big_stack_async(
-            [
-                =,
-                rvi = to_vector(realVariables),
-                ivi = to_vector(integerVariables),
-                bvi = to_vector(booleanVariables),
-                svi = to_vector(stringVariables)
-            ]() {
-                try {
-                    auto rva = gsl::make_span(realBuffer_);
-                    auto iva = gsl::make_span(integerBuffer_);
-                    auto bva = gsl::make_span(booleanBuffer_);
-                    auto sva = gsl::make_span(stringBuffer_);
-                    slave_->get_real_variables(gsl::make_span(rvi), rva);
-                    slave_->get_integer_variables(gsl::make_span(ivi), iva);
-                    slave_->get_boolean_variables(gsl::make_span(bvi), bva);
-                    slave_->get_string_variables(gsl::make_span(svi), sva);
-                    state_ = oldState;
-                    return variable_values{rva, iva, bva, sva};
-                } catch (...) {
-                    state_ = slave_state::error;
-                    throw;
-                }
-            });
+        [
+            =,
+            _ = state_guard(state_),
+            rvi = to_vector(realVariables),
+            ivi = to_vector(integerVariables),
+            bvi = to_vector(booleanVariables),
+            svi = to_vector(stringVariables)
+        ] {
+            auto rva = gsl::make_span(realBuffer_);
+            auto iva = gsl::make_span(integerBuffer_);
+            auto bva = gsl::make_span(booleanBuffer_);
+            auto sva = gsl::make_span(stringBuffer_);
+            slave_->get_real_variables(gsl::make_span(rvi), rva);
+            slave_->get_integer_variables(gsl::make_span(ivi), iva);
+            slave_->get_boolean_variables(gsl::make_span(bvi), bva);
+            slave_->get_string_variables(gsl::make_span(svi), sva);
+            return variable_values{rva, iva, bva, sva};
+        });
     }
 
     boost::fibers::future<void> set_variables(
@@ -218,43 +250,54 @@ public:
         CSE_PRECONDITION(
             state_ == slave_state::initialisation ||
             state_ == slave_state::simulation);
-        const auto oldState = state_;
-        state_ = slave_state::indeterminate;
 
         return big_stack_async(
-            [
-                =,
-                rvi = to_vector(realVariables),
-                rva = to_vector(realValues),
-                ivi = to_vector(integerVariables),
-                iva = to_vector(integerValues),
-                bvi = to_vector(booleanVariables),
-                bva = to_vector(booleanValues),
-                svi = to_vector(stringVariables),
-                sva = to_vector(stringValues)
-            ]() {
-                try {
-                    // NOTE: We don't handle nonfatal_bad_value correctly here.
-                    // All functions should get called, and the exceptions should get merged.
-                    // TODO: This check is at the moment necessary for some tests to pass. Should maybe find an alternative solution!
-                    if (!rvi.empty()) {
-                        slave_->set_real_variables(gsl::make_span(rvi), gsl::make_span(rva));
-                    }
-                    if (!ivi.empty()) {
-                        slave_->set_integer_variables(gsl::make_span(ivi), gsl::make_span(iva));
-                    }
-                    if (!bvi.empty()) {
-                        slave_->set_boolean_variables(gsl::make_span(bvi), gsl::make_span(bva));
-                    }
-                    if (!svi.empty()) {
-                        slave_->set_string_variables(gsl::make_span(svi), gsl::make_span(sva));
-                    }
-                    state_ = oldState;
-                } catch (...) {
-                    state_ = slave_state::error;
-                    throw;
-                }
-            });
+        [
+            =,
+            stateGuard = state_guard(state_),
+            rvi = to_vector(realVariables),
+            rva = to_vector(realValues),
+            ivi = to_vector(integerVariables),
+            iva = to_vector(integerValues),
+            bvi = to_vector(booleanVariables),
+            bva = to_vector(booleanValues),
+            svi = to_vector(stringVariables),
+            sva = to_vector(stringValues)
+        ]() mutable {
+            // `nonfatal_bad_value` gets special treatment here. Since it's supposed
+            // to be non-fatal, it should not prevent the setting of other variable
+            // types, and it should not set the state to `error`.
+            bool nonfatalBadValue = false;
+            std::stringstream errMsg;
+            try {
+                slave_->set_real_variables(gsl::make_span(rvi), gsl::make_span(rva));
+            } catch (const nonfatal_bad_value& e) {
+                nonfatalBadValue = true;
+                errMsg << e.what() << std::endl;
+            }
+            try {
+                slave_->set_integer_variables(gsl::make_span(ivi), gsl::make_span(iva));
+            } catch (const nonfatal_bad_value& e) {
+                nonfatalBadValue = true;
+                errMsg << e.what() << std::endl;
+            }
+            try {
+                slave_->set_boolean_variables(gsl::make_span(bvi), gsl::make_span(bva));
+            } catch (const nonfatal_bad_value& e) {
+                nonfatalBadValue = true;
+                errMsg << e.what() << std::endl;
+            }
+            try {
+                slave_->set_string_variables(gsl::make_span(svi), gsl::make_span(sva));
+            } catch (const nonfatal_bad_value& e) {
+                nonfatalBadValue = true;
+                errMsg << e.what() << std::endl;
+            }
+            if (nonfatalBadValue) {
+                stateGuard.reset();
+                throw nonfatal_bad_value(errMsg.str());
+            }
+        });
     }
 
     // clang-format on
