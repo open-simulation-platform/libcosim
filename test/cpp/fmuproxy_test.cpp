e01fab709f1cb572d787a8b0696aca26a10621fb
Lars Ivar Hatledal <laht@hials.no>
2019-01-30 16:53:17 +0100

    Update fmuproxy_test

--- test/cpp/fmuproxy_test.cpp
+++ test/cpp/fmuproxy_test.cpp
@@ -8,14 +8,22 @@ using namespace cse::fmuproxy;
 
 int main() {
 
+    const double dt = 1.0/100;
+    cse::time_point t = cse::to_time_point(0);
     const std::string fmuId = "{06c2700b-b39c-4895-9151-304ddde28443}";
 
     remote_fmu fmu(fmuId, "localhost", 9090);
     auto md = fmu.model_description();
 
-    std::cout << "Model name=" << md->name << std::endl;
+    std::cout << "modelName=" << md->name << std::endl;
 
     auto slave = fmu.instantiate_slave();
+    slave->setup(t, {}, {});
+    slave->start_simulation();
+
+    slave->do_step(t, cse::to_duration(dt));
+
+    slave->end_simulation();
 
     return 0;
 
