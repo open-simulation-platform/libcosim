9145862a6debac9e4061870634ed12dd1507b2ac
eidekrist <kristoffer.eide@dnvgl.com>
2019-02-01 15:19:25 +0100

    #163 Starting basic parsing of scenario.

--- src/cpp/scenario_parser.cpp
+++ src/cpp/scenario_parser.cpp
@@ -13,6 +13,18 @@ namespace cse {
         i >> j;
         std::cout << j.dump(2) << std::endl;
 
+        if (j.count("events")) {
+            std::cout << "We found events" << std::endl;
+            auto events = j.at("events");
+            for (auto& event : events) {
+                std::cout << "Here is an event: " << event <<  std::endl;
+                auto trigger = event.at("trigger");
+                std::cout << "Trigger is; " << trigger << std::endl;
+                auto time = trigger.at("time");
+                std::cout << "Time for triggering; " << time << std::endl;
+            }
+        }
+
         return scenario::scenario();
     }
 }
