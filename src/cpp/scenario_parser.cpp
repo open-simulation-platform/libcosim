2a9d3336065438335d565b41f04d6b5157671eca
eidekrist <kristoffer.eide@dnvgl.com>
2019-02-06 15:37:27 +0100

    #163 Changed format of scenario file. Static keys for more betterer parsing.

--- src/cpp/scenario_parser.cpp
+++ src/cpp/scenario_parser.cpp
@@ -26,30 +26,32 @@ std::pair<cse::simulator_index, cse::simulator*> find_simulator(const std::unord
 
 cse::variable_type find_variable_type(const nlohmann::json& j)
 {
-    if (j.count("real")) {
+    auto typestr = j.get<std::string>();
+    if (typestr == "real") {
         return variable_type::real;
-    } else if (j.count("integer")) {
+    } else if (typestr == "integer") {
         return variable_type::integer;
-    } else if (j.count("boolean")) {
+    } else if (typestr == "boolean") {
         return variable_type::boolean;
-    } else if (j.count("string")) {
+    } else if (typestr == "string") {
         return variable_type::string;
     }
     throw std::invalid_argument("Can't process unknown variable type");
 }
 
-std::pair<cse::variable_causality, std::string> find_causality_and_name(const nlohmann::json& j)
+cse::variable_causality find_causality(const nlohmann::json& j)
 {
-    if (j.count("output")) {
-        return std::make_pair(variable_causality::output, j.at("output").get<std::string>());
-    } else if (j.count("input")) {
-        return std::make_pair(variable_causality::input, j.at("input").get<std::string>());
-    } else if (j.count("parameter")) {
-        return std::make_pair(variable_causality::parameter, j.at("parameter").get<std::string>());
-    } else if (j.count("calculatedParameter")) {
-        return std::make_pair(variable_causality::calculated_parameter, j.at("calculatedParameter").get<std::string>());
-    } else if (j.count("local")) {
-        return std::make_pair(variable_causality::local, j.at("local").get<std::string>());
+    auto caus = j.get<std::string>();
+    if (caus == "output") {
+        return variable_causality::output;
+    } else if (caus == "input") {
+        return variable_causality::input;
+    } else if (caus == "parameter") {
+        return variable_causality::parameter;
+    } else if (caus == "calculatedParameter") {
+        return variable_causality::calculated_parameter;
+    } else if (caus == "local") {
+        return variable_causality::local;
     }
     throw std::invalid_argument("Can't process unknown variable type");
 }
@@ -68,12 +70,12 @@ cse::variable_index find_variable_index(const std::vector<variable_description>&
 }
 
 template<typename T>
-std::function<T(T)> generate_manipulator(const std::string& kind, const nlohmann::json& valueRep)
+std::function<T(T)> generate_manipulator(const std::string& kind, const nlohmann::json& event)
 {
     if ("reset" == kind) {
         return nullptr;
     }
-    T value = valueRep.get<T>();
+    T value = event.at("value").get<T>();
     if ("bias" == kind) {
         return [value](T original) { return original + value; };
     } else if ("override" == kind) {
@@ -110,42 +112,25 @@ cse::scenario::variable_action get_integer_action(const std::function<int(int)>&
     }
 }
 
-} // namespace
-
-
-scenario::event generate_event(const nlohmann::json& j, std::string mode, int id, scenario::time_trigger tr, const std::unordered_map<simulator_index, simulator*>& simulators)
+cse::scenario::variable_action generate_action(const nlohmann::json& event, cse::simulator_index sim, cse::variable_type type, cse::variable_causality causality, cse::variable_index var)
 {
-
-    auto model = j.at("model");
-    auto indexAndSim = find_simulator(simulators, model);
-    simulator_index index = indexAndSim.first;
-    simulator* sim = indexAndSim.second;
-
-    variable_type type = find_variable_type(j);
-    auto causalityAndName = find_causality_and_name(j);
-    variable_causality causality = causalityAndName.first;
-    std::string varName = causalityAndName.second;
-
-    variable_index varIndex = find_variable_index(sim->model_description().variables, varName, type, causality);
-
-    // Generate manipulator
+    auto mode = event.at("action");
     switch (type) {
-        case variable_type::real: {
-            auto real_m = j.at("real");
-            std::function f = generate_manipulator<double>(mode, real_m);
-            scenario::variable_action a = get_real_action(f, causality, index, varIndex);
-            return cse::scenario::event{id, tr, a};
+        case cse::variable_type::real: {
+            auto f = generate_manipulator<double>(mode, event);
+            return get_real_action(f, causality, sim, var);
         }
-        case variable_type::integer: {
-            auto int_m = j.at("integer");
-            std::function f = generate_manipulator<int>(mode, int_m);
-            scenario::variable_action a = get_integer_action(f, causality, index, varIndex);
-            return cse::scenario::event{id, tr, a};
+        case cse::variable_type::integer: {
+            auto f = generate_manipulator<int>(mode, event);
+            return get_integer_action(f, causality, sim, var);
         }
         default:
-            throw std::invalid_argument("No support for this variable type yet");
+            throw std::invalid_argument("No support for this variable type");
     }
 }
+
+} // namespace
+
 
 scenario::scenario parse_scenario(boost::filesystem::path& scenarioFile, const std::unordered_map<simulator_index, simulator*>& simulators)
 {
@@ -155,28 +140,21 @@ scenario::scenario parse_scenario(boost::filesystem::path& scenarioFile, const s
 
     std::vector<scenario::event> events;
 
-    if (j.count("events")) {
-        for (auto& event : j.at("events")) {
-            auto id = event.at("id").get<int>();
-            auto trigger = event.at("trigger");
-            auto time = trigger.at("time");
-            auto triggerTime = time.get<double>();
-            scenario::time_trigger tr{to_time_point(triggerTime)};
+    for (auto& event : j.at("events")) {
+        auto id = event.at("id").get<int>();
+        auto trigger = event.at("trigger");
+        auto time = trigger.at("time");
+        auto triggerTime = time.get<double>();
+        scenario::time_trigger tr{to_time_point(triggerTime)};
 
-            auto action = event.at("action");
-            if (action.count("bias")) {
-                auto bias = action.at("bias");
-                events.emplace_back(generate_event(bias, "bias", id, tr, simulators));
-            } else if (action.count("override")) {
-                auto override = action.at("override");
-                events.emplace_back(generate_event(override, "override", id, tr, simulators));
-            } else if (action.count("reset")) {
-                auto reset = action.at("reset");
-                events.emplace_back(generate_event(reset, "reset", id, tr, simulators));
-            } else {
-                throw std::invalid_argument("Unknown variable manipulation type");
-            }
-        }
+        const auto& [index, simulator] = find_simulator(simulators, event.at("model"));
+        variable_type type = find_variable_type(event.at("type"));
+        variable_causality causality = find_causality(event.at("causality"));
+        auto varName = event.at("variable").get<std::string>();
+        variable_index varIndex = find_variable_index(simulator->model_description().variables, varName, type, causality);
+
+        scenario::variable_action a = generate_action(event, index, type, causality, varIndex);
+        events.emplace_back(scenario::event{id, tr, a});
     }
 
     return scenario::scenario{events};
