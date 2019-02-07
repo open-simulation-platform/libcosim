99e5ad7eaaf7164fd2b2bc78f022e1891a5b3e21
Lars Ivar Hatledal <laht@ntnu.no>
2019-02-07 13:58:08 +0100

    Update FindThrift

--- cmake/FindThrift.cmake
+++ cmake/FindThrift.cmake
@@ -5,8 +5,9 @@
 #   THRIFT_INCLUDE_DIRS   - where to find thrift/thrift.h
 #   THRIFT_LIBRARIES      - List of libraries when using Thrift.
 #   THRIFT_FOUND          - True if Thrift found.
+#
 
-find_path(THRIFT_INCLUDE_DIR NAMES thrift/Thrift.h)
+find_path(THRIFT_INCLUDE_DIR NAMES thrift/thrift.h)
 mark_as_advanced(THRIFT_INCLUDE_DIR)
 
 find_library(THRIFT_LIBRARY NAMES thrift thriftmd)
@@ -16,13 +17,20 @@ include(FindPackageHandleStandardArgs)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(THRIFT
         REQUIRED_VARS THRIFT_LIBRARY THRIFT_INCLUDE_DIR)
 
-if(THRIFT_FOUND)
-    set(THRIFT_LIBRARIES ${THRIFT_LIBRARY})
-    set(THRIFT_INCLUDE_DIRS ${THRIFT_INCLUDE_DIR})
-endif()
+if (THRIFT_FOUND)
 
-if (THRIFT_PRINT_VARS)
-    message ("THRIFT find script variables:")
-    message ("  THRIFT_INCLUDE_DIRS   = ${THRIFT_INCLUDE_DIRS}")
-    message ("  THRIFT_LIBRARIES      = ${THRIFT_LIBRARIES}")
-endif ()
+    set(THRIFT_INCLUDE_DIRS ${THRIFT_INCLUDE_DIR})
+
+    if (NOT THRIFT_LIBRARIES)
+        set(THRIFT_LIBRARIES ${THRIFT_LIBRARY})
+    endif()
+
+    if (NOT TARGET thrift::thrift)
+        add_library(thrift::thrift UNKNOWN IMPORTED)
+        set_target_properties(thrift::thrift PROPERTIES
+                INTERFACE_INCLUDE_DIRECTORIES "${THRIFT_INCLUDE_DIR}")
+        set_property(TARGET thrift::thrift APPEND PROPERTY
+                IMPORTED_LOCATION "${THRIFT_LIBRARY}")
+    endif()
+
+endif()
