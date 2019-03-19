528413d38e25eab362a6ba6123bfcfee5afbcd3a
Kristoffer Eide <37700866+eidekrist@users.noreply.github.com>
2019-03-19 19:10:09 +0100

    Manipulator documentation (#219)
    * #207 Adding doc for manipulator.hpp
    
    * #207 More docs. Manipulators variant shortcut thingy removed as it was not doc-friendly.

--- include/cse/scenario.hpp
+++ include/cse/scenario.hpp
@@ -15,49 +15,71 @@ namespace cse
 namespace scenario
 {
 
+/// The modification of the value of a variable with type `real`.
 struct real_manipulator
 {
+    /// A function which may be called any number of times. Can be `nullptr`.
     std::function<double(double)> f;
 };
 
+/// The modification of the value of a variable with type `integer`.
 struct integer_manipulator
 {
+    /// A function which may be called any number of times. Can be `nullptr`.
     std::function<int(int)> f;
 };
 
+/// The modification of the value of a variable with type `boolean`.
 struct boolean_manipulator
 {
+    /// A function which may be called any number of times. Can be `nullptr`.
     std::function<bool(bool)> f;
 };
 
+/// The modification of the value of a variable with type `string`.
 struct string_manipulator
 {
+    /// A function which may be called any number of times. Can be `nullptr`.
     std::function<std::string(std::string_view)> f;
 };
 
-using manipulators = std::variant<
-    real_manipulator,
-    integer_manipulator,
-    boolean_manipulator,
-    string_manipulator>;
-
+/// A struct specifying a variable and the modification of its value.
 struct variable_action
 {
+    /// The simulator index.
     simulator_index simulator;
+    /// The variable index.
     variable_index variable;
-    manipulators manipulator;
+    /// The modification to be done to the variable's value.
+    std::variant<
+        real_manipulator,
+        integer_manipulator,
+        boolean_manipulator,
+        string_manipulator>
+        manipulator;
+    /**
+     * Flag which should be set to `true` if the variable is an *input* to the
+     * slave (i.e. causality input or parameter), or `false` if the variable is
+     * an *output* from a slave (i.e. causality output or calculatedParameter).
+     */
     bool is_input;
 };
 
+/// A struct representing an event.
 struct event
 {
+    /// The time point at which the event should trigger.
     time_point time;
+    /// Something which should happen to a variable.
     variable_action action;
 };
 
+/// A struct representing an executable scenario.
 struct scenario
 {
+    /// A collection of time-based events.
     std::vector<event> events;
+    /// An optional time point at which the scenario should terminate.
     std::optional<time_point> end;
 };
 
--- include/cse/scenario_parser.hpp
+++ include/cse/scenario_parser.hpp
@@ -8,7 +8,18 @@
 
 namespace cse
 {
-scenario::scenario parse_scenario(const boost::filesystem::path& scenarioFile, const std::unordered_map<simulator_index, simulator*>& simulators);
-}
+/**
+ * Parses a scenario from file.
+ *
+ * \param scenarioFile
+ *  The path to a proprietary `json` file defining the scenario.
+ *
+ * \param simulators
+ *  A map containing the simulators currently loaded in the execution.
+ */
+scenario::scenario parse_scenario(
+    const boost::filesystem::path& scenarioFile,
+    const std::unordered_map<simulator_index, simulator*>& simulators);
+} // namespace cse
 
 #endif //CSECORE_SCENARIO_PARSER_H
