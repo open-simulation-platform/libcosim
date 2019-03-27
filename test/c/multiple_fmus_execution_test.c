a531c5e7e954defa07b33701ae5c3f09d893d8c2
eidekrist <kristoffer.eide@dnvgl.com>
2019-03-27 10:36:34 +0100

    #197 Almost died

--- test/c/multiple_fmus_execution_test.c
+++ test/c/multiple_fmus_execution_test.c
@@ -21,207 +21,139 @@ void print_last_error()
 
 int main()
 {
+    int exitCode = 0;
+
+    cse_execution* execution = NULL;
+    cse_slave* slave1 = NULL;
+    cse_slave* slave2 = NULL;
+    cse_observer* observer1 = NULL;
+    cse_observer* observer2 = NULL;
+    cse_manipulator* manipulator = NULL;
+
     const char* dataDir = getenv("TEST_DATA_DIR");
     if (!dataDir) {
         fprintf(stderr, "Environment variable TEST_DATA_DIR not set\n");
-        return 1;
+        goto Lfailure;
     }
 
-    int exitCode = 0;
     char fmuPath[1024];
     int rc = snprintf(fmuPath, sizeof fmuPath, "%s/fmi1/identity.fmu", dataDir);
     if (rc < 0) {
         perror(NULL);
-        return 1;
+        goto Lfailure;
     }
 
     int64_t nanoStepSize = (int64_t)(0.1 * 1.0e9);
-    cse_execution* execution = cse_execution_create(0, nanoStepSize);
-    if (!execution) {
-        print_last_error();
-        return 1;
-    }
+    execution = cse_execution_create(0, nanoStepSize);
+    if (!execution) { goto Lerror; }
 
-    cse_slave* slave1 = cse_local_slave_create(fmuPath);
-    if (!slave1) {
-        print_last_error();
-        return 1;
-    }
-    cse_slave* slave2 = cse_local_slave_create(fmuPath);
-    if (!slave2) {
-        print_last_error();
-        return 1;
-    }
+    slave1 = cse_local_slave_create(fmuPath);
+    if (!slave1) { goto Lerror; }
 
-    cse_observer* observer1 = cse_last_value_observer_create();
-    if (!observer1) {
-        print_last_error();
-        return 1;
-    }
+    slave2 = cse_local_slave_create(fmuPath);
+    if (!slave2) { goto Lerror; }
 
-    cse_observer* observer2 = cse_last_value_observer_create();
-    if (!observer2) {
-        print_last_error();
-        return 1;
-    }
+    observer1 = cse_last_value_observer_create();
+    if (!observer1) { goto Lerror; }
 
-    cse_manipulator* manipulator = cse_override_manipulator_create();
-    if (!manipulator) {
-        print_last_error();
-        return 1;
-    }
+    observer2 = cse_last_value_observer_create();
+    if (!observer2) { goto Lerror; }
+
+    manipulator = cse_override_manipulator_create();
+    if (!manipulator) { goto Lerror; }
 
     rc = cse_execution_add_manipulator(execution, manipulator);
-    if (rc < 0) {
-        print_last_error();
-        cse_execution_destroy(execution);
-        return 1;
-    }
+    if (rc < 0) { goto Lerror; }
 
     cse_slave_index slave_index1 = cse_execution_add_slave(execution, slave1);
-    if (slave_index1 < 0) {
-        print_last_error();
-        cse_execution_destroy(execution);
-        return 1;
-    }
+    if (slave_index1 < 0) { goto Lerror; }
     cse_slave_index slave_index2 = cse_execution_add_slave(execution, slave2);
-    if (slave_index2 < 0) {
-        print_last_error();
-        cse_execution_destroy(execution);
-        return 1;
-    }
+    if (slave_index2 < 0) { goto Lerror; }
 
     rc = cse_execution_add_observer(execution, observer1);
-    if (rc < 0) {
-        print_last_error();
-        cse_execution_destroy(execution);
-        return 1;
-    }
+    if (rc < 0) { goto Lerror; }
 
     rc = cse_execution_add_observer(execution, observer2);
-    if (rc < 0) {
-        print_last_error();
-        cse_execution_destroy(execution);
-        return 1;
-    }
+    if (rc < 0) { goto Lerror; }
 
 
     cse_variable_index realInVar = 0;
     const double realInVal = 5.0;
     rc = cse_manipulator_slave_set_real(manipulator, slave_index1, &realInVar, 1, &realInVal);
-    if (rc < 0) {
-        print_last_error();
-        cse_execution_destroy(execution);
-        return 1;
-    }
+    if (rc < 0) { goto Lerror; }
 
     cse_variable_index intInVar = 0;
     const int intInVal = 42;
     rc = cse_manipulator_slave_set_integer(manipulator, slave_index1, &intInVar, 1, &intInVal);
-    if (rc < 0) {
-        print_last_error();
-        cse_execution_destroy(execution);
-        return 1;
-    }
+    if (rc < 0) { goto Lerror; }
 
     rc = cse_execution_step(execution, 10);
-    if (rc < 0) {
-        print_last_error();
-        cse_execution_destroy(execution);
-        return 1;
-    }
+    if (rc < 0) { goto Lerror; }
 
     cse_execution_status executionStatus;
     rc = cse_execution_get_status(execution, &executionStatus);
-    if (rc < 0) {
-        print_last_error();
-        cse_execution_destroy(execution);
-        return 1;
-    }
+    if (rc < 0) { goto Lerror; }
 
     double precision = 1e-9;
     double simTime = executionStatus.current_time * 1e-9;
     if (fabs(simTime - 1.0) > precision) {
         fprintf(stderr, "Expected current time == 1.0 s, got %f\n", simTime);
-        cse_execution_destroy(execution);
-        return 1;
+        goto Lfailure;
     }
 
     if (executionStatus.state != CSE_EXECUTION_STOPPED) {
         fprintf(stderr, "Expected state == %i, got %i\n", CSE_EXECUTION_STOPPED, executionStatus.state);
-        cse_execution_destroy(execution);
-        return 1;
+        goto Lfailure;
     }
 
     if (executionStatus.error_code != CSE_ERRC_SUCCESS) {
         fprintf(stderr, "Expected error code == %i, got %i\n", CSE_ERRC_SUCCESS, executionStatus.error_code);
-        cse_execution_destroy(execution);
-        return 1;
+        goto Lfailure;
     }
 
     cse_variable_index realOutVar = 0;
     double realOutVal = -1.0;
     rc = cse_observer_slave_get_real(observer1, slave_index1, &realOutVar, 1, &realOutVal);
-    if (rc < 0) {
-        print_last_error();
-        cse_execution_destroy(execution);
-        return 1;
-    }
+    if (rc < 0) { goto Lerror; }
 
     cse_variable_index intOutVar = 0;
     int intOutVal = 10;
     rc = cse_observer_slave_get_integer(observer1, slave_index1, &intOutVar, 1, &intOutVal);
-    if (rc < 0) {
-        print_last_error();
-        cse_execution_destroy(execution);
-        return 1;
-    }
+    if (rc < 0) { goto Lerror; }
 
     if (realOutVal != 5.0) {
         fprintf(stderr, "Expected value 0.0, got %f\n", realOutVal);
-        cse_execution_destroy(execution);
-        return 1;
+        goto Lfailure;
     }
     if (intOutVal != 42) {
         fprintf(stderr, "Expected value 0, got %i\n", intOutVal);
-        cse_execution_destroy(execution);
-        return 1;
+        goto Lfailure;
     }
 
     rc = cse_observer_slave_get_real(observer2, slave_index2, &realOutVar, 1, &realOutVal);
-    if (rc < 0) {
-        print_last_error();
-        cse_execution_destroy(execution);
-        return 1;
-    }
+    if (rc < 0) { goto Lerror; }
 
     rc = cse_observer_slave_get_integer(observer2, slave_index2, &intOutVar, 1, &intOutVal);
-    if (rc < 0) {
-        print_last_error();
-        cse_execution_destroy(execution);
-        return 1;
-    }
+    if (rc < 0) { goto Lerror; }
 
     if (realOutVal != 0.0) {
         fprintf(stderr, "Expected value 0.0, got %f\n", realOutVal);
-        cse_execution_destroy(execution);
-        return 1;
+        goto Lfailure;
     }
     if (intOutVal != 0) {
         fprintf(stderr, "Expected value 0, got %i\n", intOutVal);
-        cse_execution_destroy(execution);
-        return 1;
+        goto Lfailure;
     }
 
     goto Lcleanup;
 
-//  Lerror:
-//    print_last_error();
-//
-//  Lfailure:
-//    exitCode = 1;
+Lerror:
+    print_last_error();
 
-  Lcleanup:
+Lfailure:
+    exitCode = 1;
+
+Lcleanup:
     cse_manipulator_destroy(manipulator);
     cse_observer_destroy(observer1);
     cse_observer_destroy(observer2);
--- test/c/observer_can_buffer_samples.c
+++ test/c/observer_can_buffer_samples.c
@@ -22,77 +22,65 @@ void print_last_error()
 
 int main()
 {
+    int exitCode = 0;
+
+    cse_execution* execution = NULL;
+    cse_slave* slave = NULL;
+    cse_observer* observer = NULL;
+    cse_manipulator* manipulator = NULL;
+
     const char* dataDir = getenv("TEST_DATA_DIR");
     if (!dataDir) {
         fprintf(stderr, "Environment variable TEST_DATA_DIR not set\n");
-        return 1;
+        goto Lfailure;
     }
 
     char fmuPath[1024];
     int rc = snprintf(fmuPath, sizeof fmuPath, "%s/fmi1/identity.fmu", dataDir);
     if (rc < 0) {
         perror(NULL);
-        return 1;
+        goto Lfailure;
     }
 
     int64_t nanoStepSize = (int64_t)(0.1 * 1.0e9);
-    cse_execution* execution = cse_execution_create(0, nanoStepSize);
-    if (!execution) {
-        print_last_error();
-        return 1;
-    }
+    execution = cse_execution_create(0, nanoStepSize);
+    if (!execution) { goto Lerror; }
 
-    cse_slave* slave = cse_local_slave_create(fmuPath);
-    if (!slave) {
-        print_last_error();
-        return 1;
-    }
+    slave = cse_local_slave_create(fmuPath);
+    if (!slave) { goto Lerror; }
 
-    cse_observer* observer = cse_time_series_observer_create();
-    if (!observer) {
-        print_last_error();
-        return 1;
-    }
+    observer = cse_time_series_observer_create();
+    if (!observer) { goto Lerror; }
 
-    cse_slave_index slave_index = cse_execution_add_slave(execution, slave);
-    if (slave_index < 0) {
-        print_last_error();
-        cse_execution_destroy(execution);
-        return 1;
-    }
+    cse_slave_index slaveIndex = cse_execution_add_slave(execution, slave);
+    if (slaveIndex < 0) { goto Lerror; }
 
     rc = cse_execution_add_observer(execution, observer);
-    if (rc < 0) {
-        print_last_error();
-        cse_execution_destroy(execution);
-        return 1;
-    }
+    if (rc < 0) { goto Lerror; }
 
-    cse_manipulator* manipulator = cse_override_manipulator_create();
-    if (!manipulator) {
-        print_last_error();
-        return 1;
-    }
+    manipulator = cse_override_manipulator_create();
+    if (!manipulator) { goto Lerror; }
 
     rc = cse_execution_add_manipulator(execution, manipulator);
-    if (rc < 0) {
-        print_last_error();
-        cse_execution_destroy(execution);
-        return 1;
-    }
+    if (rc < 0) { goto Lerror; }
 
     double inputRealSamples[10] = {0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0};
     int inputIntSamples[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
     cse_variable_index index = 0;
 
-    cse_observer_start_observing(observer, slave_index, CSE_REAL, index);
-    cse_observer_start_observing(observer, slave_index, CSE_INTEGER, index);
+    rc = cse_observer_start_observing(observer, slaveIndex, CSE_REAL, index);
+    if (rc < 0) { goto Lerror; }
+    rc = cse_observer_start_observing(observer, slaveIndex, CSE_INTEGER, index);
+    if (rc < 0) { goto Lerror; }
 
     for (int i = 0; i < 10; i++) {
-        cse_manipulator_slave_set_real(manipulator, 0, &index, 1, &inputRealSamples[i]);
-        cse_manipulator_slave_set_integer(manipulator, 0, &index, 1, &inputIntSamples[i]);
-        cse_execution_step(execution, 1);
+        rc = cse_manipulator_slave_set_real(manipulator, 0, &index, 1, &inputRealSamples[i]);
+        if (rc < 0) { goto Lerror; }
+        rc = cse_manipulator_slave_set_integer(manipulator, 0, &index, 1, &inputIntSamples[i]);
+        if (rc < 0) { goto Lerror; }
+        rc = cse_execution_step(execution, 1);
+        if (rc < 0) { goto Lerror; }
     }
 
     cse_step_number fromStep = 1;
@@ -102,20 +90,18 @@ int main()
     cse_time_point times[10];
     cse_step_number steps[10];
 
-    int64_t readRealSamples = cse_observer_slave_get_real_samples(observer, slave_index, index, fromStep, nSamples, realSamples, steps, times);
+    int64_t readRealSamples = cse_observer_slave_get_real_samples(observer, slaveIndex, index, fromStep, nSamples, realSamples, steps, times);
     if (readRealSamples != (int64_t)nSamples) {
         print_last_error();
         fprintf(stderr, "Expected to read 10 real samples, got %" PRId64 "\n", readRealSamples);
-        cse_execution_destroy(execution);
-        return 1;
+        goto Lfailure;
     }
 
-    int64_t readIntSamples = cse_observer_slave_get_integer_samples(observer, slave_index, index, fromStep, nSamples, intSamples, steps, times);
+    int64_t readIntSamples = cse_observer_slave_get_integer_samples(observer, slaveIndex, index, fromStep, nSamples, intSamples, steps, times);
     if (readIntSamples != (int64_t)nSamples) {
         print_last_error();
         fprintf(stderr, "Expected to read 10 int samples, got %" PRId64 "\n", readIntSamples);
-        cse_execution_destroy(execution);
-        return 1;
+        goto Lfailure;
     }
 
     long expectedSteps[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
@@ -127,59 +113,64 @@ int main()
         expectedTimeSamples[j] = (cse_time_point)(1.0e9 * t[j]);
     }
 
-
     for (int i = 0; i < 10; i++) {
         if (fabs(expectedRealSamples[i] - realSamples[i]) > 0.000001) {
             fprintf(stderr, "Sample nr %d expected real sample %lf, got %lf\n", i, expectedRealSamples[i], realSamples[i]);
-            cse_execution_destroy(execution);
-            return 1;
+            goto Lfailure;
         }
         if (expectedIntSamples[i] != intSamples[i]) {
             fprintf(stderr, "Sample nr %d expected int sample %d, got %d\n", i, expectedIntSamples[i], intSamples[i]);
-            cse_execution_destroy(execution);
-            return 1;
+            goto Lfailure;
         }
         if (expectedSteps[i] != steps[i]) {
             fprintf(stderr, "Sample nr %d expected step %li, got %lli\n", i, expectedSteps[i], steps[i]);
-            cse_execution_destroy(execution);
-            return 1;
+            goto Lfailure;
         }
         if (expectedTimeSamples[i] != times[i]) {
             fprintf(stderr, "Sample nr %d expected time sample %" PRId64 ", got %" PRId64 "\n", i, expectedTimeSamples[i], times[i]);
-            cse_execution_destroy(execution);
-            return 1;
+            goto Lfailure;
         }
     }
 
     cse_step_number nums[2];
     cse_duration dur = (cse_time_point)(0.5 * 1.0e9);
-    cse_observer_get_step_numbers_for_duration(observer, 0, dur, nums);
+    rc = cse_observer_get_step_numbers_for_duration(observer, 0, dur, nums);
+    if (rc < 0) { goto Lerror; }
     if (nums[0] != 5) {
         fprintf(stderr, "Expected step number %i, got %lli\n", 5, nums[0]);
-        cse_execution_destroy(execution);
-        return 1;
+        goto Lfailure;
     }
     if (nums[1] != 10) {
         fprintf(stderr, "Expected step number %i, got %lli\n", 10, nums[1]);
-        cse_execution_destroy(execution);
-        return 1;
+        goto Lfailure;
     }
 
     cse_time_point t1 = (cse_time_point)(0.3 * 1e9);
     cse_time_point t2 = (cse_time_point)(0.6 * 1e9);
-    cse_observer_get_step_numbers(observer, 0, t1, t2, nums);
+    rc = cse_observer_get_step_numbers(observer, 0, t1, t2, nums);
+    if (rc < 0) { goto Lerror; }
     if (nums[0] != 3) {
         fprintf(stderr, "Expected step number %i, got %lli\n", 3, nums[0]);
-        cse_execution_destroy(execution);
-        return 1;
+        goto Lfailure;
     }
     if (nums[1] != 6) {
         fprintf(stderr, "Expected step number %i, got %lli\n", 6, nums[1]);
-        cse_execution_destroy(execution);
-        return 1;
+        goto Lfailure;
     }
 
+    goto Lcleanup;
 
+Lerror:
+    print_last_error();
+
+Lfailure:
+    exitCode = 1;
+
+Lcleanup:
+    cse_manipulator_destroy(manipulator);
+    cse_observer_destroy(observer);
+    cse_local_slave_destroy(slave);
     cse_execution_destroy(execution);
-    return 0;
+
+    return exitCode;
 }
