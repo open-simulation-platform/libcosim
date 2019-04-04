82742c8cf7a521bb6077e46949d1a83f4027b736
Lars T. Kyllingstad <lars.kyllingstad@sintef.no>
2019-04-04 15:08:50 +0200

    Remove event_loop and libevent
    It's not used at the moment and can be reintroduced if necessary.

--- conanfile.py
+++ conanfile.py
@@ -18,17 +18,15 @@ class CSECoreConan(ConanFile):
         "boost/1.66.0@conan/stable",
         "FMILibrary/2.0.3@kyllingstad/testing",
         "gsl_microsoft/1.0.0@bincrafters/stable",
-        "libevent/2.0.22@bincrafters/stable",
         "libzip/1.5.1@bincrafters/stable",
         "jsonformoderncpp/3.5.0@vthiery/stable"
         )
-    
+
     default_options = (
         "boost:shared=True",
-        "libevent:with_openssl=False",
         "libzip:shared=True"
         )
-    
+
     def imports(self):
         binDir = os.path.join("output", str(self.settings.build_type).lower(), "bin")
         self.copy("*.dll", dst=binDir, keep_path=False)
