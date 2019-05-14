25e521980fc7640b778ed6dfd8ef7cb3d4e7baa5
Lars Ivar Hatledal <laht@ntnu.no>
2019-05-14 08:52:44 +0200

    Remove unit test for fmu-proxy. Will fail without a server available.

--- test/cpp/orchestration_unittest.cpp
+++ test/cpp/orchestration_unittest.cpp
@@ -1,9 +1,6 @@
 #define BOOST_TEST_MODULE orchestration.hpp unittests
 #include <cse/orchestration.hpp>
 #include <cse/uri.hpp>
-#ifdef WITH_FMUPROXY
-#    include <cse/fmuproxy/fmuproxy_uri_sub_resolver.hpp>
-#endif
 
 #include <boost/filesystem.hpp>
 #include <boost/test/unit_test.hpp>
@@ -37,22 +34,3 @@ BOOST_AUTO_TEST_CASE(file_uri_sub_resolver_relative_path_test)
     auto model = resolver.lookup_model(baseUri, "fmi2/Clock.fmu");
     BOOST_TEST(model->description()->name == "Clock");
 }
-
-#ifdef WITH_FMUPROXY
-
-BOOST_AUTO_TEST_CASE(fmuproxy_uri_sub_resolver_test)
-{
-    const auto testDataDir = std::getenv("TEST_DATA_DIR");
-    BOOST_TEST_REQUIRE(!!testDataDir);
-
-    std::string uri = "fmu-proxy://localhost:9090?file=" + std::string(testDataDir) + "/fmi2/Clock.fmu";
-
-    cse::model_uri_resolver resolver;
-    resolver.add_sub_resolver(std::make_shared<cse::fmuproxy::fmuproxy_uri_sub_resolver>());
-
-    auto model = resolver.lookup_model("", uri);
-
-    BOOST_TEST(model->description()->name == "Clock");
-}
-
-#endif
