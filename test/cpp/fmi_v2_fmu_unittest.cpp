e54548d06d2859722cf82af4150a0ac85b006c43
Jamt, Levi Kristian <Levi.Jamt@dnvgl.com>
2019-05-15 11:11:53 +0200

    #244 removing curly brackets from unpack fmu dir path

--- test/cpp/fmi_v2_fmu_unittest.cpp
+++ test/cpp/fmi_v2_fmu_unittest.cpp
@@ -27,6 +27,7 @@ BOOST_AUTO_TEST_CASE(v2_fmu)
     BOOST_TEST(d->author.empty());
     BOOST_TEST(d->version.empty());
     BOOST_TEST(std::static_pointer_cast<fmi::v2::fmu>(fmu)->fmilib_handle() != nullptr);
+    BOOST_TEST(std::static_pointer_cast<fmi::v2::fmu>(fmu)->directory().filename() == "ad6d7bad-97d1-4fb9-ab3e-00a0d051e42c");
 
     auto instance = fmu->instantiate_slave("testSlave");
     instance->setup(
