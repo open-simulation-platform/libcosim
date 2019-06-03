266690c6552179ff04e7ca25ef8dc781e7a6e186
Lars Ivar Hatledal <laht@ntnu.no>
2019-06-03 12:45:00 +0200

    Add documentation for fmu-proxy_uri_sub_resolver

--- include/cse/fmuproxy/fmuproxy_uri_sub_resolver.hpp
+++ include/cse/fmuproxy/fmuproxy_uri_sub_resolver.hpp
@@ -10,6 +10,15 @@ namespace cse
 namespace fmuproxy
 {
 
+/**
+ *  Class for resolving fmu-proxy URI schemes.
+ *
+ *  Supports file, url and guid (pre-loaded on server) FMUs
+ *
+ *  From file: `fmu-proxy://127.0.0.1:9090?file=models/my_model.fmu`
+ *  From url: `fmu-proxy://127.0.0.1:9090?url=http://example.com/my_model.fmu`
+ *  From guid: `fmu-proxy://127.0.0.1:9090?guid={ads4ffsa4523fgg8}`
+ */
 class fmuproxy_uri_sub_resolver : public model_uri_sub_resolver
 {
 
