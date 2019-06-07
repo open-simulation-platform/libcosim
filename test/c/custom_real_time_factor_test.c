7a0b0356d0889d7f4ace36d837d07bc5dea2ff17
STENBRO <Roger.Eivind.Stenbro@dnvgl.com>
2019-06-07 10:02:31 +0200

    #169 Added test

--- test/c/custom_real_time_factor_test.c
+++ test/c/custom_real_time_factor_test.c
@@ -47,14 +47,9 @@ int main() {
         perror(NULL);
         goto Lfailure;
     }
-    char fmuPath2[1024];
-    rc = snprintf(fmuPath2, sizeof fmuPath2, "%s/fmi2/WaterTank_Control.fmu", dataDir);
-    if (rc < 0) {
-        perror(NULL);
-        goto Lfailure;
-    }
 
     double stepSize = 0.1;
+    double real_time_factor = 2.25;
     int64_t nanoStepSize = (int64_t)(stepSize * 1.0e9);
 
     execution = cse_execution_create(0, nanoStepSize);
@@ -63,51 +58,35 @@ int main() {
     slave1 = cse_local_slave_create(fmuPath1);
     if (!slave1) { goto Lerror; }
 
-    slave2 = cse_local_slave_create(fmuPath2);
-    if (!slave2) { goto Lerror; }
-
     int idx1 = cse_execution_add_slave(execution, slave1);
     if (idx1 < 0) { goto Lerror; }
-    int idx2 = cse_execution_add_slave(execution, slave2);
-    if (idx2 < 0) { goto Lerror; }
+
+    rc = cse_execution_enable_real_time_simulation(execution);
+    if (rc < 0) { goto Lerror; }
 
     cse_execution_status executionStatus;
     rc = cse_execution_get_status(execution, &executionStatus);
     if (rc < 0) { goto Lerror; }
-    if (!executionStatus.is_real_time_simulation) {
-        cse_execution_enable_real_time_simulation(execution);
-    }
 
     rc = cse_execution_start(execution);
     if (rc < 0) { goto Lerror; }
-    int64_t before1 = GetCurrentTime();
-    Sleep(2000);
-    rc = cse_execution_stop(execution);
-    if (rc < 0) { goto Lerror; }
-    int64_t after1 = GetCurrentTime();
 
     Sleep(1000);
 
-    rc = cse_execution_start(execution);
-    if (rc < 0) { goto Lerror; }
-    int64_t before2 = GetCurrentTime();
-    Sleep(2000);
+    rc = cse_execution_set_custom_real_time_factor(execution, real_time_factor);
+
+    Sleep(4000);
     rc = cse_execution_stop(execution);
     if (rc < 0) { goto Lerror; }
-    int64_t after2 = GetCurrentTime();
-
-    int64_t actualElapsed = (after1 - before1) + (after2 - before2);
-    double elapsedS = actualElapsed * 1.0e-3;
-
-    int rounded = (int) (0.5 + elapsedS / stepSize);
-    double elapsed = rounded * stepSize;
 
     rc = cse_execution_get_status(execution, &executionStatus);
     if (rc < 0) { goto Lerror; }
 
-    double simTime = executionStatus.current_time * 1e-9;
-    if (fabs(simTime - elapsed) > 1.0e-3) {
-        fprintf(stderr, "Expected final simulation time == %f, got %f\n", elapsedS, simTime);
+    double rtf = executionStatus.real_time_factor;
+    printf("The setpoint real time factor is %f\n", real_time_factor);
+    printf("The measured real time factor is %f\n", rtf);
+    if (fabs(rtf - real_time_factor) > 1.0e-2) {
+        fprintf(stderr, "Expected real time factor == %f, got %f\n", real_time_factor, rtf);
         goto Lfailure;
     }
 
@@ -121,7 +100,6 @@ int main() {
 
     Lcleanup:
     cse_local_slave_destroy(slave2);
-    cse_local_slave_destroy(slave1);
     cse_execution_destroy(execution);
 
     return exitCode;
