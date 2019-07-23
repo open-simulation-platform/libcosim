1e5d1dbc0e19516dcc5ce579b0c6ca6ff8f5cd2a
Lars T. Kyllingstad <lars.kyllingstad@sintef.no>
2019-07-23 09:28:18 +0200

    Remove cmake.test() from conanfile build function
    Fixes #303

--- conanfile.py
+++ conanfile.py
@@ -54,7 +54,6 @@ class CSECoreConan(ConanFile):
     def build(self):
         cmake = self.configure_cmake()
         cmake.build()
-        cmake.test()
         self.run('cmake --build . --target doc')
 
     def package(self):
