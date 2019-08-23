e85c27d1af17c714b3f002d743d58f315f992397
Lars Ivar Hatledal <laht@ntnu.no>
2019-08-23 08:32:15 +0200

    Explicitly set bzip2 version

--- conanfile.py
+++ conanfile.py
@@ -19,7 +19,8 @@ class CSECoreConan(ConanFile):
         "FMILibrary/2.0.3@kyllingstad/testing",
         "gsl_microsoft/1.0.0@bincrafters/stable",
         "libzip/1.5.2@bincrafters/stable",
-        "jsonformoderncpp/3.5.0@vthiery/stable"
+        "jsonformoderncpp/3.5.0@vthiery/stable",
+        "bzip2/1.0.8@conan/stable"
         )
 
     options = {"fmuproxy": [True, False]}
