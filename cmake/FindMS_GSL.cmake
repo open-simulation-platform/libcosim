8a73d9ec09eef26d908a82316f8ec5f497bfcb3f
Lars Ivar Hatledal <laht@hials.no>
2019-08-28 17:39:42 +0200

    Rename gsl -> ms_gsl and GSL -> MS_GSL

--- cmake/FindMS_GSL.cmake
+++ cmake/FindMS_GSL.cmake
@@ -1,17 +1,17 @@
 # Finds the core guidelines support library.
 #
 # If found, the following is defined:
-#   - `GSL_FOUND` and `GSL_INCLUDE_DIRS` variables.
-#   - `gsl` target
+#   - `MS_GSL_FOUND` and `MS_GSL_INCLUDE_DIRS` variables.
+#   - `ms_gsl` target
 
-find_path(GSL_INCLUDE_DIR "gsl/gsl")
-if(GSL_INCLUDE_DIR)
-    set(GSL_INCLUDE_DIRS "${GSL_INCLUDE_DIR}")
-    add_library(gsl INTERFACE IMPORTED)
-    set_target_properties(gsl PROPERTIES
-        INTERFACE_INCLUDE_DIRECTORIES "${GSL_INCLUDE_DIR}"
+find_path(MS_GSL_INCLUDE_DIR "gsl/gsl")
+if(MS_GSL_INCLUDE_DIR)
+    set(MS_GSL_INCLUDE_DIRS "${MS_GSL_INCLUDE_DIR}")
+    add_library(ms_gsl INTERFACE IMPORTED)
+    set_target_properties(ms_gsl PROPERTIES
+        INTERFACE_INCLUDE_DIRECTORIES "${MS_GSL_INCLUDE_DIR}"
     )
 endif()
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args("GSL" DEFAULT_MSG "GSL_INCLUDE_DIRS")
+find_package_handle_standard_args("MS_GSL" DEFAULT_MSG "MS_GSL_INCLUDE_DIRS")
