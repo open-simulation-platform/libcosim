20afac3a350e2ad2b0e8e7089764046b77fad362
Lars Ivar Hatledal <laht@ntnu.no>
2019-09-03 09:30:51 +0200

    Use declared namespace

--- src/cpp/fmuproxy/fmuproxy_client.cpp
+++ src/cpp/fmuproxy/fmuproxy_client.cpp
@@ -86,7 +86,7 @@ cse::fmuproxy::fmuproxy_client::from_file(const std::string& file)
 
     BOOST_LOG_SEV(log::logger(), log::debug) << "fmu-proxy will load FMU from file '" << file << "'";
 
-    if ( !boost::filesystem::exists( file ) )
+    if ( !fs::exists( file ) )
     {
         std::string msg = "No such file '" + file +  "'!";
         CSE_PANIC_M(msg.c_str());
