bba4a76889d6ce46afd8a0fef0758be79d09f6c7
Lars Ivar Hatledal <laht@hials.no>
2019-09-24 22:25:45 +0200

    Fix ambiguity

--- include/cse/ssp_parser.hpp
+++ include/cse/ssp_parser.hpp
@@ -28,14 +28,18 @@ using simulator_map = std::map<std::string, simulator_map_entry>;
 
 std::pair<execution, simulator_map> load_ssp(
     cse::model_uri_resolver&,
+    const boost::filesystem::path& sspDir);
+
+std::pair<execution, simulator_map> load_ssp(
+    cse::model_uri_resolver&,
     const boost::filesystem::path& sspDir,
-    std::optional<cse::time_point> overrideStartTime = std::nullopt);
+    std::optional<cse::time_point> overrideStartTime);
 
 
 std::pair<execution, simulator_map> load_ssp(
     cse::model_uri_resolver&,
     const boost::filesystem::path& sspDir,
-    std::shared_ptr<cse::algorithm> overrideAlgorithm = nullptr,
+    std::shared_ptr<cse::algorithm> overrideAlgorithm,
     std::optional<cse::time_point> overrideStartTime = std::nullopt);
 
 } // namespace cse
