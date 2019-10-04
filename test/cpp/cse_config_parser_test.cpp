22151f56c558158a04b4cc99438ecf5563ca5ea0
Jamt, Levi Kristian <Levi.Jamt@dnvgl.com>
2019-10-04 12:41:38 +0200

    #400 unit test for initial values

--- test/cpp/cse_config_parser_test.cpp
+++ test/cpp/cse_config_parser_test.cpp
@@ -1,5 +1,6 @@
 #include <cse/cse_config_parser.hpp>
 #include <cse/log/simple.hpp>
+#include <cse/observer/last_value_observer.hpp>
 #include <cse/orchestration.hpp>
 
 #include <boost/filesystem.hpp>
@@ -19,14 +20,25 @@ void test(const boost::filesystem::path& configPath)
     auto resolver = cse::default_model_uri_resolver();
     auto simulation = cse::load_cse_config(*resolver, configPath, boost::filesystem::path(testDataDir) / "msmi/OspSystemStructure.xsd", cse::to_time_point(0.0));
     auto& execution = simulation.first;
-
     auto& simulator_map = simulation.second;
+
     REQUIRE(simulator_map.size() == 2);
     REQUIRE(simulator_map.at("CraneController").source == "../ssp/demo/CraneController.fmu");
     REQUIRE(simulator_map.at("KnuckleBoomCrane").source == "../ssp/demo/KnuckleBoomCrane.fmu");
 
+    auto obs = std::make_shared<cse::last_value_observer>();
+    execution.add_observer(obs);
+
     auto result = execution.simulate_until(cse::to_time_point(1e-3));
     REQUIRE(result.get());
+
+    cse::simulator_index i = simulator_map.at("CraneController").index;
+    double realValue = -1.0;
+    cse::value_reference reference = cse::find_variable(simulator_map.at("CraneController").description, "cl1_min").reference;
+    obs->get_real(i, gsl::make_span(&reference, 1), gsl::make_span(&realValue, 1));
+
+    double magicNumberFromConf = 2.2;
+    REQUIRE(std::fabs(realValue - magicNumberFromConf) < 1e-9);
 }
 
 int main()
