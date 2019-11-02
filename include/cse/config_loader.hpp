dd4d46d045b27d0da61d9ae169f044a008cce215
Lars Ivar Hatledal <laht@ntnu.no>
2019-11-02 09:37:05 +0100

    Change doc to be generic rather than specific to ssp

--- include/cse/config_loader.hpp
+++ include/cse/config_loader.hpp
@@ -30,7 +30,7 @@ public:
 
     /**
      *  Explicitly specify the simulation start time
-     *  Will override any value found in the ssp.
+     *  Will override any value found in the configuration file.
      *
      *  \param [in] timePoint
      *      The (logical) time point at which the simulation should start
@@ -39,7 +39,7 @@ public:
 
     /**
      *  Explicitly specify the co-simulation algorithm to use.
-     *  Will override any value found in the ssp.
+     *  Will override any value found in the configuration file.
      *
      *  \param [in] algorithm
      *      The co-simulation algorithm to be used in the execution.
@@ -56,10 +56,10 @@ public:
     void set_custom_model_uri_resolver(std::shared_ptr<cse::model_uri_resolver> modelResolver);
 
     /**
-     *  Creates an execution based on a SystemStructure.ssd file.
+     *  Creates an execution based on a configuration file.
      *
      *  \param [in] configPath
-     *      Path to an .ssd file, or a directory holding SystemStructure.ssd
+     *      Path to the configuration file, or the directory holding it.
      */
     virtual std::pair<execution, simulator_map> load(const boost::filesystem::path& configPath) = 0;
 
