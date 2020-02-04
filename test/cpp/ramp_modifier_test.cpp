e84f170bd809bdc4aaa31e67e16ae3760605a97f
eidekrist <kristoffer.eide@dnvgl.com>
2020-02-04 07:53:16 +0100

    Add test for output modifier as well.

--- test/cpp/ramp_modifier_test.cpp
+++ test/cpp/ramp_modifier_test.cpp
@@ -20,16 +20,18 @@
 class test_manipulator : public cse::manipulator
 {
 public:
-    explicit test_manipulator(cse::variable_id variable, cse::time_point startTime)
+    explicit test_manipulator(cse::variable_id variable, cse::time_point startTime, bool input)
         : variable_(variable)
         , acc_(0.0)
+        , input_(input)
     {
         double slope = 1.0;
         double* accumulator = &acc_;
-        f_ = std::function<double(double, cse::duration)>([=](double original, cse::duration deltaT) {
-            *accumulator += slope * cse::to_double_duration(deltaT, startTime);
-            return original + *accumulator;
-        });
+        f_ = std::function<double(double, cse::duration)>(
+            [=](double original, cse::duration deltaT) {
+                *accumulator += slope * cse::to_double_duration(deltaT, startTime);
+                return original + *accumulator;
+            });
     }
 
     void simulator_added(cse::simulator_index, cse::manipulable* man, cse::time_point) override
@@ -44,8 +46,13 @@ public:
     void step_commencing(cse::time_point) override
     {
         if (!initialized_) {
-            man_->expose_for_setting(variable_.type, variable_.reference);
-            man_->set_real_input_modifier(variable_.reference, f_);
+            if (input_) {
+                man_->expose_for_setting(variable_.type, variable_.reference);
+                man_->set_real_input_modifier(variable_.reference, f_);
+            } else {
+                man_->expose_for_getting(variable_.type, variable_.reference);
+                man_->set_real_output_modifier(variable_.reference, f_);
+            }
             initialized_ = true;
         }
     }
@@ -58,6 +65,7 @@ private:
     std::function<double(double, cse::duration)> f_;
     bool initialized_ = false;
     double acc_;
+    bool input_;
 };
 
 int main()
@@ -83,17 +91,30 @@ int main()
         const auto input = cse::variable_id{simIndex, cse::variable_type::real, 1};
         const auto output = cse::variable_id{simIndex, cse::variable_type::real, 0};
 
-        auto manipulator = std::make_shared<test_manipulator>(input, startTime);
+        auto inputManipulator = std::make_shared<test_manipulator>(input, startTime, true);
+        auto outputManipulator = std::make_shared<test_manipulator>(output, startTime, false);
 
-        execution.add_manipulator(manipulator);
+        execution.add_manipulator(inputManipulator);
+        execution.add_manipulator(outputManipulator);
 
-        double expectedValues[] = {1.1, 1.2, 1.3, 1.4, 1.5, 1.6};
-        for (double expected : expectedValues) {
+        execution.set_real_initial_value(input.simulator, input.reference, 0.0);
+
+        double expectedInputValues[] = {0.1, 0.2, 0.3, 0.4, 0.5, 0.6};
+        double expectedOutputValues[] = {0.2, 0.4, 0.6, 0.8, 1.0, 1.2};
+        for (int i = 0; i < 6; i++) {
             execution.step();
-            double value = -1.0;
-            observer->get_real(output.simulator, gsl::make_span(&output.reference, 1), gsl::make_span(&value, 1));
-            std::cout << "Expected value: " << expected << ", actual value: " << value << std::endl;
-            REQUIRE(std::fabs(expected - value) < 1.0e-9);
+
+            double expectedInputValue = expectedInputValues[i];
+            double inputValue = -1.0;
+            observer->get_real(input.simulator, gsl::make_span(&input.reference, 1), gsl::make_span(&inputValue, 1));
+            std::cout << "Expected input value: " << expectedInputValue << ", actual value: " << inputValue << std::endl;
+            REQUIRE(std::fabs(expectedInputValue - inputValue) < 1.0e-9);
+
+            double expectedOutputValue = expectedOutputValues[i];
+            double outputValue = -1.0;
+            observer->get_real(output.simulator, gsl::make_span(&output.reference, 1), gsl::make_span(&outputValue, 1));
+            std::cout << "Expected output value: " << expectedOutputValue << ", actual value: " << outputValue << std::endl;
+            REQUIRE(std::fabs(expectedOutputValue - outputValue) < 1.0e-9);
         }
 
     } catch (const std::exception& e) {
