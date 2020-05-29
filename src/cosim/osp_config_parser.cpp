28b2d59c38c66358860a39a7377f6d27690d2d84
hplatou <halvor.platou@dnvgl.com>
2020-05-29 09:56:17 +0200

    Resolving conflicting versions of MS-GSL final_act/final_action by including code directly.

--- src/cosim/osp_config_parser.cpp
+++ src/cosim/osp_config_parser.cpp
@@ -76,6 +76,29 @@ private:
     bool failed_;
 };
 
+// final_action allows you to ensure something gets run at the end of a scope
+template <class F>
+class final_action
+{
+public:
+    explicit final_action(F f) noexcept : f_(std::move(f)) {}
+
+    final_action(final_action&& other) noexcept : f_(std::move(other.f_)), invoke_(std::exchange(other.invoke_, false)) {}
+
+    final_action(const final_action&) = delete;
+    final_action& operator=(const final_action&) = delete;
+    final_action& operator=(final_action&&) = delete;
+
+    ~final_action() noexcept
+    {
+        if (invoke_) f_();
+    }
+
+private:
+    F f_;
+    bool invoke_{true};
+};
+
 class osp_config_parser
 {
 
@@ -229,7 +252,7 @@ osp_config_parser::osp_config_parser(
 {
     // Root node
     xercesc::XMLPlatformUtils::Initialize();
-    const auto xerces_cleanup = gsl::final_action([]() {
+    const auto xerces_cleanup = final_action([]() {
         xercesc::XMLPlatformUtils::Terminate();
     });
 
@@ -573,7 +596,7 @@ struct extended_model_description
     explicit extended_model_description(const boost::filesystem::path& ospModelDescription)
     {
         xercesc::XMLPlatformUtils::Initialize();
-        const auto xerces_cleanup = gsl::final_action([]() {
+        const auto xerces_cleanup = final_action([]() {
             xercesc::XMLPlatformUtils::Terminate();
         });
         const auto domImpl = xercesc::DOMImplementationRegistry::getDOMImplementation(tc("LS").get());
