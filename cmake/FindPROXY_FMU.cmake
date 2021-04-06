39a9e128cbec47787048917661d5c2e97c93d6b5
Lars Ivar Hatledal <laht@hials.no>
2021-04-06 14:23:00 +0200

    link against proxy

--- cmake/FindPROXY_FMU.cmake
+++ cmake/FindPROXY_FMU.cmake
@@ -10,19 +10,22 @@
 find_path(PROXY_FMU_INCLUDE_DIR NAMES proxyfmu/client/proxy_fmu.hpp)
 mark_as_advanced(PROXY_FMU_INCLUDE_DIR)
 
-find_library(PROXY_FMU_LIBRARY NAMES proxy-client)
-mark_as_advanced(PROXY_FMU_LIBRARY)
+find_library(PROXY_FMI_LIBRARY NAMES fmi)
+mark_as_advanced(PROXY_FMI_LIBRARY)
+
+find_library(PROXY_CLIENT_LIBRARY NAMES proxy-client)
+mark_as_advanced(PROXY_CLIENT_LIBRARY)
 
 include(FindPackageHandleStandardArgs)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(PROXY_FMU
-        REQUIRED_VARS PROXY_FMU_LIBRARY PROXY_FMU_INCLUDE_DIR)
+        REQUIRED_VARS PROXY_FMI_LIBRARY PROXY_CLIENT_LIBRARY PROXY_FMU_INCLUDE_DIR)
 
 if (PROXY_FMU_FOUND)
 
   set(PROXY_FMU_INCLUDE_DIRS ${PROXY_FMU_INCLUDE_DIR})
 
   if (NOT PROXY_FMU_LIBRARIES)
-    set(PROXY_FMU_LIBRARIES ${PROXY_FMU_LIBRARY})
+    set(PROXY_FMU_LIBRARIES ${PROXY_FMI_LIBRARY} ${PROXY_CLIENT_LIBRARY})
   endif()
 
   if (NOT TARGET proxy-server)
@@ -30,7 +33,7 @@ if (PROXY_FMU_FOUND)
     set_target_properties(proxy-server PROPERTIES
             INTERFACE_INCLUDE_DIRECTORIES "${PROXY_FMU_INCLUDE_DIR}")
     set_property(TARGET proxy-server APPEND PROPERTY
-            IMPORTED_LOCATION "${PROXY_FMU_LIBRARY}")
+            IMPORTED_LOCATION "${PROXY_CLIENT_LIBRARY}")
   endif()
 
 endif()
