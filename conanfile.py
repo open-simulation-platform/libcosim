73f8d871c861661cee018524352533df6d4af6fb
Lars Ivar Hatledal <laht@hials.no>
2021-05-05 15:24:23 +0200

    Update proxyfmu version

--- conanfile.py
+++ conanfile.py
@@ -41,7 +41,7 @@ class LibcosimConan(ConanFile):
 
     def requirements(self):
         if self.options.proxyfmu:
-            self.requires("proxyfmu/0.2.0@osp/testing")
+            self.requires("proxyfmu/0.2.1@osp/testing")
 
     def imports(self):
         binDir = os.path.join("output", str(self.settings.build_type).lower(), "bin")
