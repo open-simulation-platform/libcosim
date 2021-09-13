4981b659501fe0871551e8164ed52465cfb1a29c
msteinsto <magnus.steinsto@dnv.com>
2021-09-13 10:11:01 +0200

    Removed unnecessary space

--- src/cosim/execution.cpp
+++ src/cosim/execution.cpp
@@ -151,7 +151,7 @@ public:
             do {
                 stepSize = step();
                 timer_.sleep(currentTime_);
-            }  while (!stopped_ && !timed_out(endTime, currentTime_, stepSize));
+            } while (!stopped_ && !timed_out(endTime, currentTime_, stepSize));
             bool isStopped = stopped_;
             stopped_ = true;
             return !isStopped;
