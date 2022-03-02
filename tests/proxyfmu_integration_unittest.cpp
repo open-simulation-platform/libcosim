e0ba8612998c86d483783bda5a1b7c356aff36a0
David Heejong Park <hee.jong.park@dnv.com>
2022-03-02 12:35:03 +0100

    Performance improvements - fixed unit test 02

--- tests/proxyfmu_integration_unittest.cpp
+++ tests/proxyfmu_integration_unittest.cpp
@@ -168,7 +168,9 @@ BOOST_AUTO_TEST_CASE(test_fmi2)
             BOOST_TEST(start == 0.0);
             const auto varID = v.reference;
             double varVal = -1.0;
-            varVal = instance->get_variables(gsl::make_span(&varID, 1), {}, {}, {}).real[0];
+            cosim::slave::variable_values vars;
+            instance->get_variables(&vars, gsl::make_span(&varID, 1), {}, {}, {});
+            varVal = vars.real[0];
             BOOST_TEST(varVal == 0.0);
         } else if (v.name == "minlevel") {
             foundMinlevel = true;
@@ -178,7 +180,9 @@ BOOST_AUTO_TEST_CASE(test_fmi2)
             BOOST_TEST(start == 1.0);
             const auto varID = v.reference;
             double varVal = -1.0;
-            varVal = instance->get_variables(gsl::make_span(&varID, 1), {}, {}, {}).real[0];
+            cosim::slave::variable_values vars;
+            instance->get_variables(&vars, gsl::make_span(&varID, 1), {}, {}, {});
+            varVal = vars.real[0];
             BOOST_TEST(varVal == 1.0);
         }
     }
