5f53ad7ef7c5d95bebd51d611144525d8f08881e
David Heejong Park <hee.jong.park@dnv.com>
2022-03-22 09:57:05 +0100

    thread_pool update

--- src/cosim/utility/thread_pool.hpp
+++ src/cosim/utility/thread_pool.hpp
@@ -29,30 +29,28 @@ private:
     std::queue<std::function<void()>> work_queue_;
     std::vector<std::thread> threads_;
     std::mutex m_;
-    std::condition_variable cv_;
+    std::condition_variable cv_finished_;
+    std::condition_variable cv_worker_;
     unsigned int pending_tasks_;
 
     void worker_thread()
     {
+        std::chrono::seconds wait_duration(1);
         while (!done_) {
-
             std::unique_lock<std::mutex> lck(m_);
             if (!work_queue_.empty()) {
-
                 auto task = std::move(work_queue_.front());
                 work_queue_.pop();
-                lck.unlock();
 
+                lck.unlock();
                 task();
 
                 lck.lock();
                 pending_tasks_--;
                 lck.unlock();
-
-                cv_.notify_one();
-
+                cv_finished_.notify_one();
             } else {
-                std::this_thread::yield();
+                cv_worker_.wait_for(lck, wait_duration);
             }
         }
     }
@@ -85,7 +83,7 @@ public:
     {
         if (!threads_.empty()) {
             std::unique_lock<std::mutex> lck(m_);
-            while (pending_tasks_ > 0) cv_.wait(lck);
+            while (pending_tasks_ > 0) cv_finished_.wait(lck);
         }
     }
 
@@ -97,13 +95,14 @@ public:
             std::unique_lock<std::mutex> lck(m_);
             work_queue_.emplace(std::move(f));
             pending_tasks_++;
+            cv_worker_.notify_one();
         }
     }
 
     ~thread_pool() noexcept
     {
         done_ = true;
-
+        cv_worker_.notify_all();
         for (auto& thread : threads_) {
             if (thread.joinable()) {
                 thread.join();
