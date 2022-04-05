45a3d0b80d233ee2adf42eaca8b3f8d903af0184
levi <levi.jamt@dnvgl.com>
2022-04-05 15:48:14 +0200

    using proxyfmu/0.2.7@osp/stable

--- conanfile.py
+++ conanfile.py
@@ -52,7 +52,7 @@ class LibcosimConan(ConanFile):
 
     def requirements(self):
         if self.options.proxyfmu:
-            self.requires("proxyfmu/0.2.7@osp/testing")
+            self.requires("proxyfmu/0.2.7@osp/stable")
 
     def imports(self):
         binDir = os.path.join("output", str(self.settings.build_type).lower(), "bin")
