504ea635b3126690f6ac2b5ac84fda9574ae1e23
Lars Ivar Hatledal <laht@ntnu.no>
2022-06-27 10:46:23 +0200

    Not finding a variable in the model description is not exceptional (#700)

--- src/cosim/model_description.cpp
+++ src/cosim/model_description.cpp
@@ -11,14 +11,14 @@
 namespace cosim
 {
 
-variable_description find_variable(const model_description& description, const std::string& variable_name)
+std::optional<variable_description> find_variable(const model_description& description, const std::string& variable_name)
 {
     for (const auto& variable : description.variables) {
         if (variable.name == variable_name) {
             return variable;
         }
     }
-    throw std::invalid_argument("Can't find variable descriptor with name " + variable_name + " for model with name " + description.name);
+    return std::nullopt;
 }
 
 } // namespace cosim
