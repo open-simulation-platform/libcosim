18cccbb798f701274ceda7d940db0c0baa928df6
Roger Eivind Stenbro <Roger.Eivind.Stenbro@dnv.com>
2022-11-29 16:27:14 +0100

    changed file logger headers

--- src/cosim/observer/file_observer.cpp
+++ src/cosim/observer/file_observer.cpp
@@ -209,17 +209,66 @@ private:
 
         ss << "Time,StepCount";
 
+        // Add variable names
         for (const auto& vd : realVars_) {
-            ss << "," << vd.name << " [" << vd.reference << " " << vd.type << " " << vd.causality << "]";
+            ss << "," << vd.name;
         }
         for (const auto& vd : intVars_) {
-            ss << "," << vd.name << " [" << vd.reference << " " << vd.type << " " << vd.causality << "]";
+            ss << "," << vd.name;
         }
         for (const auto& vd : boolVars_) {
-            ss << "," << vd.name << " [" << vd.reference << " " << vd.type << " " << vd.causality << "]";
+            ss << "," << vd.name;
         }
         for (const auto& vd : stringVars_) {
-            ss << "," << vd.name << " [" << vd.reference << " " << vd.type << " " << vd.causality << "]";
+            ss << "," << vd.name;
+        }
+
+        ss << std::endl << "#,#";
+
+        // Add variable references
+        for (const auto& vd : realVars_) {
+            ss << "," << vd.reference;
+        }
+        for (const auto& vd : intVars_) {
+            ss << "," << vd.reference;
+        }
+        for (const auto& vd : boolVars_) {
+            ss << "," << vd.reference;
+        }
+        for (const auto& vd : stringVars_) {
+            ss << "," << vd.reference;
+        }
+
+        ss << std::endl << "#,#";
+
+        // Add variables types
+        for (const auto& vd : realVars_) {
+            ss << "," << vd.type;
+        }
+        for (const auto& vd : intVars_) {
+            ss << "," << vd.type;
+        }
+        for (const auto& vd : boolVars_) {
+            ss << "," << vd.type;
+        }
+        for (const auto& vd : stringVars_) {
+            ss << "," << vd.type;
+        }
+
+        ss << std::endl << "#,#";
+
+        // Add variable causalities
+        for (const auto& vd : realVars_) {
+            ss << "," << vd.causality;
+        }
+        for (const auto& vd : intVars_) {
+            ss << "," << vd.causality;
+        }
+        for (const auto& vd : boolVars_) {
+            ss << "," << vd.causality;
+        }
+        for (const auto& vd : stringVars_) {
+            ss << "," << vd.causality;
         }
 
         ss << std::endl;
