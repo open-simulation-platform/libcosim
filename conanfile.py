3b524782167ec11cee79e5de009ee383141cd942
Lars Ivar Hatledal <laht@ntnu.no>
2022-12-08 12:47:29 +0100

    Update conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -53,7 +53,7 @@ class LibcosimConan(ConanFile):
 
     def requirements(self):
         if self.options.proxyfmu:
-            self.requires("proxyfmu/0.3.0@osp/testing-undo_cmake_find_package")
+            self.requires("proxyfmu/0.3.0@osp/stable")
 
     def imports(self):
         binDir = os.path.join("output", str(self.settings.build_type).lower(), "bin")
