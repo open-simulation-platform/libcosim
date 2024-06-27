b4eae505984ed7721456d62622d9d63293be1b97
Lars T. Kyllingstad <lars.kyllingstad@sintef.no>
2024-06-27 19:37:08 +0200

    Missed a change in proxyfmu in last commit

--- src/cosim/proxy/remote_fmu.cpp
+++ src/cosim/proxy/remote_fmu.cpp
@@ -71,7 +71,6 @@ std::unique_ptr<cosim::model_description> parse_model_description(const proxyfmu
     _md->author = md.author;
     _md->name = md.modelName;
     _md->description = md.description;
-    _md->can_save_state = false; // Not supported by proxyfmu
 
     for (auto& var : md.modelVariables) {
         cosim::variable_description vd;
