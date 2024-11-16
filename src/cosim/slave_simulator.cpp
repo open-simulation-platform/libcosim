7a27b474778e076d9e94329eebb2bb584d1ac6a0
David Heejong Park <hee.jong.park@dnv.com>
2024-11-16 11:49:53 +0100

    Error fix

--- src/cosim/slave_simulator.cpp
+++ src/cosim/slave_simulator.cpp
@@ -241,7 +241,7 @@ public:
             references_filtered_.clear();
             values_filtered_.clear();
 
-            for (int i = 0; i < references_.size(); i++) {
+            for (size_t i = 0; i < references_.size(); i++) {
                 auto& ref = references_.at(i);
                 if ((*filter)(ref)) {
                     references_filtered_.push_back(ref);
