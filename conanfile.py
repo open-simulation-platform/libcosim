4f8d7b38ff88409372505c39f201d6f55226a128
David Heejong Park <hee.jong.park@dnv.com>
2025-01-28 15:46:22 +0100

    Added transitive_libs for libcbor to avoid cmake error

--- conanfile.py
+++ conanfile.py
@@ -39,7 +39,7 @@ class LibcosimConan(ConanFile):
     def requirements(self):
         self.tool_requires("cmake/[>=3.19]")
         self.requires("fmilibrary/[~2.3]")
-        self.requires("libcbor/[~0.11.0]")
+        self.requires("libcbor/[~0.11.0]", transitive_libs=True)
         self.requires("libzip/[>=1.7 <1.10]") # 1.10 deprecates some functions we use
         self.requires("ms-gsl/[>=3 <5]", transitive_headers=True)
         if self.options.proxyfmu:
