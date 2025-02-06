4457d6719a855b76838ea0d862915a1f3cfaf926
David Heejong Park <hee.jong.park@dnv.com>
2025-02-06 15:26:36 +0100

    Uses upgraded thrift/boost

--- conanfile.py
+++ conanfile.py
@@ -42,11 +42,9 @@ class LibcosimConan(ConanFile):
         self.requires("libcbor/[~0.11.0]")
         self.requires("libzip/[>=1.7 <1.10]") # 1.10 deprecates some functions we use
         self.requires("ms-gsl/[>=3 <5]", transitive_headers=True)
+        self.requires("boost/[~1.85]", transitive_headers=True, transitive_libs=True) # Required by Thrift
         if self.options.proxyfmu:
             self.requires("proxyfmu/0.3.3@osp/testing-feature_779-state-serialization")
-            self.requires("boost/[~1.81]", transitive_headers=True, transitive_libs=True) # Required by Thrift
-        else:
-            self.requires("boost/[>=1.71]", transitive_headers=True, transitive_libs=True)
         self.requires("yaml-cpp/[~0.8]")
         self.requires("xerces-c/[~3.2]")
 
