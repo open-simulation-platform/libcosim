898991ed524d445f216eaa3aaeb99763f0b5c80f
David Heejong Park <hee.jong.park@dnv.com>
2025-05-12 13:29:13 +0200

    Using updated proxyfmu

--- conanfile.py
+++ conanfile.py
@@ -47,7 +47,7 @@ class LibcosimConan(ConanFile):
         self.requires("ms-gsl/[>=3 <5]", transitive_headers=True)
         self.requires("boost/[~1.85]", transitive_headers=True, transitive_libs=True)  # Required by Thrift
         if self.options.proxyfmu:
-            self.requires("proxyfmu/0.3.3@osp/testing",
+            self.requires("proxyfmu/0.4.1@osp/testing-frame-size-fix",
                           transitive_headers=True,
                           transitive_libs=True)
         self.requires("yaml-cpp/[~0.8]")
