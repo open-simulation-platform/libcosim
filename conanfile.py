a45b6343ef991e6303814a6bbf05d09e66db2a07
Hee Jong Park <hee.jong.park@dnv.com>
2026-09-21 08:24:59 +0200

    Refactor CI configuration and update Conan package upload steps

--- conanfile.py
+++ conanfile.py
@@ -47,7 +47,7 @@ class LibcosimConan(ConanFile):
         self.requires("ms-gsl/[>=3 <5]", transitive_headers=True)
         self.requires("boost/[~1.85]", transitive_headers=True, transitive_libs=True)  # Required by Thrift
         if self.options.proxyfmu:
-            self.requires("proxyfmu/0.4.3@osp/testing-fmilibrary-upgrade",
+            self.requires("proxyfmu/0.4.3@osp/testing",
                           transitive_headers=True,
                           transitive_libs=True)
         self.requires("yaml-cpp/[~0.9]")
